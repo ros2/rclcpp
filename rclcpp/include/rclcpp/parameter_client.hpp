2917aed993dffd46ff665e428c0e6477638b1538
Esteve Fernandez <esteve@apache.org>
2015-06-25 11:49:33 -0700

    Added on_parameter_event to SyncParametersClient

--- rclcpp/include/rclcpp/parameter_client.hpp
+++ rclcpp/include/rclcpp/parameter_client.hpp
@@ -229,7 +229,7 @@ public:
 
   template<typename FunctorT>
   typename rclcpp::subscription::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr
-  on_parameter_event(FunctorT callback)
+  on_parameter_event(FunctorT & callback)
   {
     // TODO(esteve): remove hardcoded values
     return node_->create_subscription<rcl_interfaces::msg::ParameterEvent>("parameter_events",
@@ -306,6 +306,13 @@ public:
   {
     auto f = async_parameters_client_->list_parameters(parameter_prefixes, depth);
     return rclcpp::executors::spin_node_until_future_complete(*executor_, node_, f).get();
+  }
+
+  template<typename FunctorT>
+  typename rclcpp::subscription::Subscription<rcl_interfaces::msg::ParameterEvent>::SharedPtr
+  on_parameter_event(FunctorT & callback)
+  {
+    return async_parameters_client_->on_parameter_event(callback);
   }
 
 private:
