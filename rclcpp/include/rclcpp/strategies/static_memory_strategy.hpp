6d389a9456eba2bc84b8c0da54719cae8c9573d5
Jackie Kay <jackie@osrfoundation.org>
2015-08-13 11:18:04 -0700

    implement method chaining

--- rclcpp/include/rclcpp/strategies/static_memory_strategy.hpp
+++ rclcpp/include/rclcpp/strategies/static_memory_strategy.hpp
@@ -30,18 +30,53 @@ namespace static_memory_strategy
 struct ObjectPoolBounds
 {
 public:
-  size_t max_subscriptions_;
-  size_t max_services_;
-  size_t max_clients_;
-  size_t max_executables_;
-  size_t max_guard_conditions_;
-  size_t pool_size_;
+  size_t max_subscriptions;
+  size_t max_services;
+  size_t max_clients;
+  size_t max_executables;
+  size_t max_guard_conditions;
+  size_t pool_size;
 
-  ObjectPoolBounds(size_t subs = 10, size_t services = 10, size_t clients = 10,
-    size_t executables = 1, size_t guard_conditions = 2, size_t pool = 1024)
-  : max_subscriptions_(subs), max_services_(services), max_clients_(clients),
-    max_executables_(executables), max_guard_conditions_(guard_conditions), pool_size_(pool)
+  ObjectPoolBounds()
+  : max_subscriptions(10), max_services(10), max_clients(10),
+    max_executables(1), max_guard_conditions(2), pool_size(1024)
   {}
+
+  ObjectPoolBounds & set_max_subscriptions(size_t subscriptions)
+  {
+    max_subscriptions = subscriptions;
+    return *this;
+  }
+
+  ObjectPoolBounds & set_max_services(size_t services)
+  {
+    max_services = services;
+    return *this;
+  }
+
+  ObjectPoolBounds & set_max_clients(size_t clients)
+  {
+    max_clients = clients;
+    return *this;
+  }
+
+  ObjectPoolBounds & set_max_guard_conditions(size_t guard_conditions)
+  {
+    max_guard_conditions = guard_conditions;
+    return *this;
+  }
+
+  ObjectPoolBounds & set_max_executables(size_t executables)
+  {
+    max_executables = executables;
+    return *this;
+  }
+
+  ObjectPoolBounds & set_memory_pool_size(size_t pool)
+  {
+    pool_size = pool;
+    return *this;
+  }
 };
 
 
@@ -52,36 +87,36 @@ public:
   : bounds_(bounds), memory_pool_(nullptr), subscription_pool_(nullptr),
     service_pool_(nullptr), guard_condition_pool_(nullptr), executable_pool_(nullptr)
   {
-    if (bounds_.pool_size_) {
-      memory_pool_ = new void *[bounds_.pool_size_];
-      memset(memory_pool_, 0, bounds_.pool_size_ * sizeof(void *));
+    if (bounds_.pool_size) {
+      memory_pool_ = new void *[bounds_.pool_size];
+      memset(memory_pool_, 0, bounds_.pool_size * sizeof(void *));
     }
 
-    if (bounds_.max_subscriptions_) {
-      subscription_pool_ = new void *[bounds_.max_subscriptions_];
-      memset(subscription_pool_, 0, bounds_.max_subscriptions_ * sizeof(void *));
+    if (bounds_.max_subscriptions) {
+      subscription_pool_ = new void *[bounds_.max_subscriptions];
+      memset(subscription_pool_, 0, bounds_.max_subscriptions * sizeof(void *));
     }
 
-    if (bounds_.max_services_) {
-      service_pool_ = new void *[bounds_.max_services_];
-      memset(service_pool_, 0, bounds_.max_services_ * sizeof(void *));
+    if (bounds_.max_services) {
+      service_pool_ = new void *[bounds_.max_services];
+      memset(service_pool_, 0, bounds_.max_services * sizeof(void *));
     }
 
-    if (bounds_.max_clients_) {
-      client_pool_ = new void *[bounds_.max_clients_];
-      memset(client_pool_, 0, bounds_.max_clients_ * sizeof(void *));
+    if (bounds_.max_clients) {
+      client_pool_ = new void *[bounds_.max_clients];
+      memset(client_pool_, 0, bounds_.max_clients * sizeof(void *));
     }
 
-    if (bounds_.max_guard_conditions_) {
-      guard_condition_pool_ = new void *[bounds_.max_guard_conditions_];
-      memset(guard_condition_pool_, 0, bounds_.max_guard_conditions_ * sizeof(void *));
+    if (bounds_.max_guard_conditions) {
+      guard_condition_pool_ = new void *[bounds_.max_guard_conditions];
+      memset(guard_condition_pool_, 0, bounds_.max_guard_conditions * sizeof(void *));
     }
 
-    if (bounds_.max_executables_) {
-      executable_pool_ = new executor::AnyExecutable::SharedPtr[bounds_.max_executables_];
+    if (bounds_.max_executables) {
+      executable_pool_ = new executor::AnyExecutable::SharedPtr[bounds_.max_executables];
     }
 
-    for (size_t i = 0; i < bounds_.max_executables_; ++i) {
+    for (size_t i = 0; i < bounds_.max_executables; ++i) {
       executable_pool_[i] = std::make_shared<executor::AnyExecutable>();
     }
 
@@ -89,27 +124,27 @@ public:
     exec_seq_ = 0;
 
     // Reserve pool_size_ buckets in the memory map.
-    memory_map_.reserve(bounds_.pool_size_);
-    for (size_t i = 0; i < bounds_.pool_size_; ++i) {
+    memory_map_.reserve(bounds_.pool_size);
+    for (size_t i = 0; i < bounds_.pool_size; ++i) {
       memory_map_[memory_pool_[i]] = 0;
     }
   }
 
   ~StaticMemoryStrategy()
   {
-    if (bounds_.pool_size_) {
+    if (bounds_.pool_size) {
       delete[] memory_pool_;
     }
-    if (bounds_.max_subscriptions_) {
+    if (bounds_.max_subscriptions) {
       delete[] subscription_pool_;
     }
-    if (bounds_.max_services_) {
+    if (bounds_.max_services) {
       delete[] service_pool_;
     }
-    if (bounds_.max_clients_) {
+    if (bounds_.max_clients) {
       delete[] client_pool_;
     }
-    if (bounds_.max_guard_conditions_) {
+    if (bounds_.max_guard_conditions) {
       delete[] guard_condition_pool_;
     }
   }
@@ -118,25 +153,25 @@ public:
   {
     switch (type) {
       case HandleType::subscription_handle:
-        if (number_of_handles > bounds_.max_subscriptions_) {
+        if (number_of_handles > bounds_.max_subscriptions) {
           throw std::runtime_error("Requested size exceeded maximum subscriptions.");
         }
 
         return subscription_pool_;
       case HandleType::service_handle:
-        if (number_of_handles > bounds_.max_services_) {
+        if (number_of_handles > bounds_.max_services) {
           throw std::runtime_error("Requested size exceeded maximum services.");
         }
 
         return service_pool_;
       case HandleType::client_handle:
-        if (number_of_handles > bounds_.max_clients_) {
+        if (number_of_handles > bounds_.max_clients) {
           throw std::runtime_error("Requested size exceeded maximum clients.");
         }
 
         return client_pool_;
       case HandleType::guard_condition_handle:
-        if (number_of_handles > bounds_.max_guard_conditions_) {
+        if (number_of_handles > bounds_.max_guard_conditions) {
           throw std::runtime_error("Requested size exceeded maximum guard_conditions.");
         }
 
@@ -152,23 +187,23 @@ public:
     (void)handles;
     switch (type) {
       case HandleType::subscription_handle:
-        if (bounds_.max_subscriptions_) {
-          memset(subscription_pool_, 0, bounds_.max_subscriptions_ * sizeof(void *));
+        if (bounds_.max_subscriptions) {
+          memset(subscription_pool_, 0, bounds_.max_subscriptions * sizeof(void *));
         }
         break;
       case HandleType::service_handle:
-        if (bounds_.max_services_) {
-          memset(service_pool_, 0, bounds_.max_services_ * sizeof(void *));
+        if (bounds_.max_services) {
+          memset(service_pool_, 0, bounds_.max_services * sizeof(void *));
         }
         break;
       case HandleType::client_handle:
-        if (bounds_.max_clients_) {
-          memset(client_pool_, 0, bounds_.max_clients_ * sizeof(void *));
+        if (bounds_.max_clients) {
+          memset(client_pool_, 0, bounds_.max_clients * sizeof(void *));
         }
         break;
       case HandleType::guard_condition_handle:
-        if (bounds_.max_guard_conditions_) {
-          memset(guard_condition_pool_, 0, bounds_.max_guard_conditions_ * sizeof(void *));
+        if (bounds_.max_guard_conditions) {
+          memset(guard_condition_pool_, 0, bounds_.max_guard_conditions * sizeof(void *));
         }
         break;
       default:
@@ -178,7 +213,7 @@ public:
 
   executor::AnyExecutable::SharedPtr instantiate_next_executable()
   {
-    if (exec_seq_ >= bounds_.max_executables_) {
+    if (exec_seq_ >= bounds_.max_executables) {
       // wrap around
       exec_seq_ = 0;
     }
@@ -203,7 +238,7 @@ public:
   {
     // Extremely naive static allocation strategy
     // Keep track of block size at a given pointer
-    if (pool_seq_ + size > bounds_.pool_size_) {
+    if (pool_seq_ + size > bounds_.pool_size) {
       // Start at 0
       pool_seq_ = 0;
     }
