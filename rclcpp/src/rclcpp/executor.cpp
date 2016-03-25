b508cd79a82ccaada0d0870c5e4c5b94612ca0cb
Jackie Kay <jackie@osrfoundation.org>
2016-03-25 11:42:18 -0700

    Clean up guard conditions properly

--- rclcpp/src/rclcpp/executor.cpp
+++ rclcpp/src/rclcpp/executor.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <algorithm>
 #include <string>
 #include <type_traits>
 
@@ -412,6 +413,10 @@ Executor::wait_for_work(std::chrono::nanoseconds timeout)
   if (status != RMW_RET_OK && status != RMW_RET_TIMEOUT) {
     throw std::runtime_error(rmw_get_error_string_safe());
   }
+  guard_cond_handles_.erase(
+      std::remove(guard_cond_handles_.begin(), guard_cond_handles_.end(), nullptr),
+      guard_cond_handles_.end()
+  );
 
   memory_strategy_->remove_null_handles();
 }
