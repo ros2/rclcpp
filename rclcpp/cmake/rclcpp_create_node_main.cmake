74505f25fa1057811cff23f51b1a3dda4d97b52d
Dirk Thomas <dthomas@osrfoundation.org>
2016-11-07 14:45:44 -0800

    add rclcpp_register_node_plugins macro

--- rclcpp/cmake/rclcpp_create_node_main.cmake
+++ rclcpp/cmake/rclcpp_create_node_main.cmake
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# copied from rclcpp/rclcpp-extras.cmake
-
 
 set(rclcpp_node_main_SRC "${rclcpp_DIR}/../../../src/rclcpp/node_main.cpp")
 
--- rclcpp/rclcpp-extras.cmake
+++ rclcpp/rclcpp-extras.cmake
@@ -14,5 +14,17 @@
 
 # copied from rclcpp/rclcpp-extras.cmake
 
+# register ament_package() hook for node plugins once
+macro(_rclcpp_register_package_hook)
+  if(NOT DEFINED _RCLCPP_PACKAGE_HOOK_REGISTERED)
+    set(_RCLCPP_PACKAGE_HOOK_REGISTERED TRUE)
+
+    find_package(ament_cmake_core QUIET REQUIRED)
+    ament_register_extension("ament_package" "rclcpp"
+      "rclcpp_package_hook.cmake")
+  endif()
+endmacro()
+
 include("${rclcpp_DIR}/get_rclcpp_information.cmake")
 include("${rclcpp_DIR}/rclcpp_create_node_main.cmake")
+include("${rclcpp_DIR}/rclcpp_register_node_plugins.cmake")
