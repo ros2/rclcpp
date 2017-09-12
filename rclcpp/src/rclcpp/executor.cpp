036a0090a2153648da7b09efb239b138ea723e61
dhood <dhood@users.noreply.github.com>
2017-09-11 18:08:17 -0700

    Check for the client error code, not server

--- rclcpp/src/rclcpp/executor.cpp
+++ rclcpp/src/rclcpp/executor.cpp
@@ -333,7 +333,7 @@ Executor::execute_client(
     response.get());
   if (status == RCL_RET_OK) {
     client->handle_response(request_header, response);
-  } else if (status != RCL_RET_SERVICE_TAKE_FAILED) {
+  } else if (status != RCL_RET_CLIENT_TAKE_FAILED) {
     fprintf(stderr,
       "[rclcpp::error] take response failed for client of service '%s': %s\n",
       client->get_service_name().c_str(), rcl_get_error_string_safe());
