f7a1e668d72434b787e5713a91ece9a0c6f08182
dhood <dhood@users.noreply.github.com>
2017-11-29 10:47:32 -0800

    Documentation

--- rclcpp/include/rclcpp/logger.hpp
+++ rclcpp/include/rclcpp/logger.hpp
@@ -20,9 +20,15 @@
 
 #include "rclcpp/visibility_control.hpp"
 
-// When this define evaluates to true (default), logger factory functions will be enabled.
-// When false, logger factory functions will create dummy loggers to avoid computational expense.
-// This should be used in combination with `RCLCPP_LOG_MIN_SEVERITY` to compile out logging macros.
+/**
+ * \def RCLCPP_LOGGING_ENABLED
+ * When this define evaluates to true (default), logger factory functions will
+ * behave normally.
+ * When false, logger factory functions will create dummy loggers to avoid
+ * computational expense in manipulating objects.
+ * This should be used in combination with `RCLCPP_LOG_MIN_SEVERITY` to compile
+ * out logging macros.
+ */
 // TODO(dhood): determine this automatically from `RCLCPP_LOG_MIN_SEVERITY`
 #ifndef RCLCPP_LOGGING_ENABLED
 #define RCLCPP_LOGGING_ENABLED 1
@@ -36,10 +42,18 @@ class Logger
 private:
   friend Logger rclcpp::get_logger(const std::string & name);
 
-  // Constructors are private to force use of factory functions.
+  /// Constructor of a dummy logger.
+  /**
+   * This is used when logging is disabled: see `RCLCPP_LOGGING_ENABLED`.
+   * This cannot be called directly, see `rclcpp::get_logger` instead.
+   */
   Logger()
   : name_(nullptr) {}
 
+  /// Constructor of a named logger.
+  /**
+   * This cannot be called directly, see `rclcpp::get_logger` instead.
+   */
   explicit Logger(const std::string & name)
   : name_(new std::string(name)) {}
 
@@ -49,6 +63,12 @@ public:
   RCLCPP_PUBLIC
   Logger(const Logger &) = default;
 
+  /// Get the name of this logger.
+  /**
+   * \return the full name of the logger including any prefixes, or
+   * \return `nullptr` if this logger is invalid (e.g. because logging is
+   *   disabled).
+   */
   RCLCPP_PUBLIC
   const char * get_name() const
   {
@@ -58,21 +78,45 @@ public:
     return name_->c_str();
   }
 
+  /// Return a logger that is a descendant of this logger.
+  /**
+   * The child logger's full name will include any hierarchy conventions that
+   * indicate it is a descendant of this logger.
+   * For example, ```get_logger('abc').get_child('def')``` will return a logger
+   * with name `abc.def`.
+   *
+   * \param[in] suffix the child logger's suffix
+   * \return a logger with the fully-qualified name including the suffix, or
+   * \return a dummy logger if this logger is invalid (e.g. because logging is
+   *   disabled).
+   */
   RCLCPP_PUBLIC
-  Logger get_child(const std::string & child_name)
+  Logger get_child(const std::string & suffix)
   {
     if (!name_) {
       return Logger();
     }
-    return Logger(*name_ + "." + child_name);
+    return Logger(*name_ + "." + suffix);
   }
 };
 
+/// Return a named logger.
+/**
+ * The returned logger's name will include any naming conventions, such as a
+ * name prefix.
+ * Currently there are no such naming conventions but they may be introduced in
+ * the future.
+ *
+ * \param[in] name the name of the logger
+ * \return a logger with the fully-qualified name including naming conventions, or
+ * \return a dummy logger if logging is disabled.
+ */
 inline Logger get_logger(const std::string & name)
 {
-#ifdef RCLCPP_LOGGING_ENABLED
+#if RCLCPP_LOGGING_ENABLED
   return rclcpp::Logger(name);
 #else
+  (void)name;
   return rclcpp::Logger();
 #endif
 }
@@ -80,7 +124,17 @@ inline Logger get_logger(const std::string & name)
 namespace logging_macro_utilities
 {
 
-/// Helper function to give useful compiler errors in logging macros if passed incorrect type.
+/// Helper function to give useful compiler errors in logging macros.
+/**
+ * This is not intended for regular use: the `Logger::get_name` method should be
+ * used.
+ * This will provide a compiler error that includes the `Logger` class if an
+ * incorrect type is passed as a parameter, e.g. through incorrect usage of
+ * logging macros such as `RCLCPP_INFO()`.
+ *
+ * \param[in] logger the logger to get the name of
+ * \return the name of the logger
+ */
 inline const char * get_logger_name(const Logger & logger)
 {
   return logger.get_name();
