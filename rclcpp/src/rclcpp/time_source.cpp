a0649de6f77067537d61ff39b97c6d1ef18d32ff
Mikael Arguedas <mikael@osrfoundation.org>
2018-07-05 19:14:41 -0700

    fix indentation to comply with uncrusity 0.67

--- rclcpp/src/rclcpp/time_source.cpp
+++ rclcpp/src/rclcpp/time_source.cpp
@@ -92,7 +92,7 @@ void TimeSource::attachNode(
     node_topics_,
     node_graph_,
     node_services_
-    );
+  );
   parameter_subscription_ =
     parameter_client_->on_parameter_event(std::bind(&TimeSource::on_parameter_event,
       this, std::placeholders::_1));
