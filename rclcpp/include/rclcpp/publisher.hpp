7e5b4168b402f50b576fe7d94d2e82cb8adb7479
Karsten Knese <karsten.knese@googlemail.com>
2018-08-24 13:58:37 -0500

    const parameter

--- rclcpp/include/rclcpp/publisher.hpp
+++ rclcpp/include/rclcpp/publisher.hpp
@@ -290,7 +290,7 @@ public:
   }
 
   void
-  publish(std::shared_ptr<rcl_serialized_message_t> serialized_msg)
+  publish(std::shared_ptr<const rcl_serialized_message_t> serialized_msg)
   {
     return this->publish(serialized_msg.get());
   }
