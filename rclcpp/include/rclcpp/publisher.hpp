22e44cf03bf97f86c5632cb538ad8c46534df13f
Michael Carroll <michael@openrobotics.org>
2019-05-01 11:56:33 -0500

    Adjust for allocation in serialized message method.
    Signed-off-by: Michael Carroll <michael@openrobotics.org>

--- rclcpp/include/rclcpp/publisher.hpp
+++ rclcpp/include/rclcpp/publisher.hpp
@@ -158,7 +158,7 @@ public:
       // TODO(Karsten1987): support serialized message passed by intraprocess
       throw std::runtime_error("storing serialized messages in intra process is not supported yet");
     }
-    auto status = rcl_publish_serialized_message(&publisher_handle_, serialized_msg);
+    auto status = rcl_publish_serialized_message(&publisher_handle_, serialized_msg, nullptr);
     if (RCL_RET_OK != status) {
       rclcpp::exceptions::throw_from_rcl_error(status, "failed to publish serialized message");
     }
