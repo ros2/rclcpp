265f5653b8c21a1f13224221a8bb8a9fd502d524
Alex <cvbn127@gmail.com>
2019-08-04 16:16:11 +0400

    added throwing parameter name if parameter is not set
    Signed-off-by: Alex <cvbn127@gmail.com>

--- rclcpp/include/rclcpp/parameter_client.hpp
+++ rclcpp/include/rclcpp/parameter_client.hpp
@@ -271,7 +271,7 @@ public:
   {
     return get_parameter_impl(
       parameter_name,
-      std::function<T()>([]() -> T {throw std::runtime_error("Parameter not set");}));
+      std::function<T()>([]() -> T {throw std::runtime_error("Parameter '" + parameter_name + "' is not set");}));
   }
 
   RCLCPP_PUBLIC
