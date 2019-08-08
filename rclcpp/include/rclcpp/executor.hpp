9ea65b17a46960a1a7a20773a333980ffded8c44
Dan Rose <dan@digilabs.io>
2019-08-08 13:39:05 -0500

    .
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- rclcpp/include/rclcpp/executor.hpp
+++ rclcpp/include/rclcpp/executor.hpp
@@ -212,7 +212,7 @@ public:
   /// Spin (blocking) until the future is complete, it times out waiting, or rclcpp is interrupted.
   /**
    * \param[in] future The future to wait on. If this function returns SUCCESS, the future can be
-   *   accessed without blocking (though it may still throw an exception)
+   *   accessed without blocking (though it may still throw an exception).
    * \param[in] timeout Optional timeout parameter, which gets passed to Executor::spin_node_once.
    *   `-1` is block forever, `0` is non-blocking.
    *   If the time spent inside the blocking loop exceeds this timeout, return a TIMEOUT return
