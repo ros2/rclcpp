9b4f04927759b1e33f6b2d365fbed5cedb4e883b
Hunter L. Allen <hunterlallen@protonmail.com>
2019-10-04 19:07:26 -0400

    Fix Compiler Warning (#881)
    Signed-off-by: Hunter L. Allen <hunterlallen@protonmail.com>

--- rclcpp/include/rclcpp/message_memory_strategy.hpp
+++ rclcpp/include/rclcpp/message_memory_strategy.hpp
@@ -98,9 +98,9 @@ public:
     auto serialized_msg = std::shared_ptr<rcl_serialized_message_t>(
       msg,
       [](rmw_serialized_message_t * msg) {
-        auto ret = rmw_serialized_message_fini(msg);
+        auto fini_ret = rmw_serialized_message_fini(msg);
         delete msg;
-        if (ret != RCL_RET_OK) {
+        if (fini_ret != RCL_RET_OK) {
           RCUTILS_LOG_ERROR_NAMED(
             "rclcpp",
             "failed to destroy serialized message: %s", rcl_get_error_string().str);
