7b7a0da46a14ad7f7423d1c66644947883138408
Brian Ezequiel Marchi <brian.marchi65@gmail.com>
2019-10-09 15:45:55 -0300

    Revert "Create throttle macro for rclcpp"
    This reverts commit 1b78e76c58aab0aac91cc3a6a6d6539a8ce8d7e7.
    
    Signed-off-by: Brian Ezequiel Marchi <brian.marchi65@gmail.com>

--- rclcpp/src/rclcpp/clock.cpp
+++ rclcpp/src/rclcpp/clock.cpp
@@ -52,7 +52,7 @@ Clock::now()
 {
   Time now(0, 0, rcl_clock_.type);
 
-  auto ret = rcl_clock_get_now(&rcl_clock_, &now.rcl_time_.nanoseconds);
+  auto ret = get_now_as_timepoint(&now.rcl_time_.nanoseconds);
   if (ret != RCL_RET_OK) {
     exceptions::throw_from_rcl_error(ret, "could not get current time stamp");
   }
@@ -139,6 +139,17 @@ Clock::create_jump_callback(
     }
   });
   // *INDENT-ON*
+}
+
+rcutils_ret_t
+Clock::get_now_as_timepoint(
+  rcutils_time_point_value_t * now)
+{
+  auto ret = rcl_clock_get_now(&rcl_clock_, now);
+  if (ret != RCL_RET_OK) {
+    exceptions::throw_from_rcl_error(ret, "could not get current time stamp");
+  }
+  return RCUTILS_RET_OK;
 }
 
 }  // namespace rclcpp
