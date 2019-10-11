ca559de1d8e6416e13fa4daf3771b0f622770d6d
Christophe Bedard <christophe.bedard@apex.ai>
2019-10-11 16:30:19 -0700

    Rely on get_symbol overload instead of using enable_if
    Signed-off-by: Christophe Bedard <christophe.bedard@apex.ai>

--- rclcpp/include/rclcpp/timer.hpp
+++ rclcpp/include/rclcpp/timer.hpp
@@ -139,9 +139,10 @@ public:
       rclcpp_timer_callback_added,
       (const void *)get_timer_handle().get(),
       (const void *)&callback_);
-    // TODO(christophebedard) maybe there's a simpler way
-    // to treat callback_ as an std::function here explicitly
-    this->register_callback_for_tracing();
+    TRACEPOINT(
+      rclcpp_callback_register,
+      (const void *)&callback_,
+      get_symbol(callback_));
   }
 
   /// Default destructor.
@@ -201,39 +202,6 @@ protected:
   RCLCPP_DISABLE_COPY(GenericTimer)
 
   FunctorT callback_;
-
-private:
-  template<
-    typename CallbackT = FunctorT,
-    typename std::enable_if<
-      rclcpp::function_traits::same_arguments<CallbackT, VoidCallbackType>::value
-    >::type * = nullptr
-  >
-  void
-  register_callback_for_tracing()
-  {
-    VoidCallbackType function = callback_;
-    TRACEPOINT(
-      rclcpp_callback_register,
-      (const void *)&callback_,
-      get_symbol(function));
-  }
-
-  template<
-    typename CallbackT = FunctorT,
-    typename std::enable_if<
-      rclcpp::function_traits::same_arguments<CallbackT, TimerCallbackType>::value
-    >::type * = nullptr
-  >
-  void
-  register_callback_for_tracing()
-  {
-    TimerCallbackType function = callback_;
-    TRACEPOINT(
-      rclcpp_callback_register,
-      (const void *)&callback_,
-      get_symbol(function));
-  }
 };
 
 template<
