d5b8831a42ced22401cb2571ded48857b7339846
Todd Malsbary <todd.malsbary@intel.com>
2019-11-06 13:36:41 -0800

    Intra-process subscriber should use RMW actual qos. (ros2#913)
    Signed-off-by: Todd Malsbary <todd.malsbary@intel.com>

--- rclcpp/include/rclcpp/subscription.hpp
+++ rclcpp/include/rclcpp/subscription.hpp
@@ -124,15 +124,16 @@ public:
       using rclcpp::detail::resolve_intra_process_buffer_type;
 
       // Check if the QoS is compatible with intra-process.
-      if (qos.get_rmw_qos_profile().history == RMW_QOS_POLICY_HISTORY_KEEP_ALL) {
+      rmw_qos_profile_t qos_profile = get_actual_qos().get_rmw_qos_profile();
+      if (qos_profile.history == RMW_QOS_POLICY_HISTORY_KEEP_ALL) {
         throw std::invalid_argument(
                 "intraprocess communication is not allowed with keep all history qos policy");
       }
-      if (qos.get_rmw_qos_profile().depth == 0) {
+      if (qos_profile.depth == 0) {
         throw std::invalid_argument(
                 "intraprocess communication is not allowed with 0 depth qos policy");
       }
-      if (qos.get_rmw_qos_profile().durability != RMW_QOS_POLICY_DURABILITY_VOLATILE) {
+      if (qos_profile.durability != RMW_QOS_POLICY_DURABILITY_VOLATILE) {
         throw std::invalid_argument(
                 "intraprocess communication allowed only with volatile durability");
       }
@@ -149,7 +150,7 @@ public:
         options.get_allocator(),
         context,
         this->get_topic_name(),    // important to get like this, as it has the fully-qualified name
-        qos.get_rmw_qos_profile(),
+        qos_profile,
         resolve_intra_process_buffer_type(options.intra_process_buffer_type, callback)
         );
 
