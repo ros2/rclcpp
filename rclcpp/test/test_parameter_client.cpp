b25213a186add0ee93e270d08c9cb5601ae88066
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-01-13 15:08:19 -0800

    fix style from #963

--- rclcpp/test/test_parameter_client.cpp
+++ rclcpp/test/test_parameter_client.cpp
@@ -69,8 +69,8 @@ TEST_F(TestParameterClient, async_construction_and_destruction) {
   {
     ASSERT_THROW(
     {
-      auto asynchronous_client =
-        std::make_shared<rclcpp::AsyncParametersClient>(node, "invalid_remote_node?");
+      auto asynchronous_client = std::make_shared<rclcpp::AsyncParametersClient>(
+        node, "invalid_remote_node?");
       (void)asynchronous_client;
     }, rclcpp::exceptions::InvalidServiceNameError);
   }
@@ -105,8 +105,8 @@ TEST_F(TestParameterClient, sync_construction_and_destruction) {
   {
     ASSERT_THROW(
     {
-      auto synchronous_client =
-        std::make_shared<rclcpp::SyncParametersClient>(node, "invalid_remote_node?");
+      auto synchronous_client = std::make_shared<rclcpp::SyncParametersClient>(
+        node, "invalid_remote_node?");
       (void)synchronous_client;
     }, rclcpp::exceptions::InvalidServiceNameError);
   }
