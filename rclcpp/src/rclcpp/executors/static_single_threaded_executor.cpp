0ccc766e562487d0fed8aa348f5d87bf92753c38
Mauro <mpasserino@irobot.com>
2020-03-03 18:29:15 +0000

    Remove not needed comparison
    wait_set_.size_of_* is always different than '0'
    if we are inside the for loop
    
    Signed-off-by: Mauro <mpasserino@irobot.com>

--- rclcpp/src/rclcpp/executors/static_single_threaded_executor.cpp
+++ rclcpp/src/rclcpp/executors/static_single_threaded_executor.cpp
@@ -217,7 +217,7 @@ StaticSingleThreadedExecutor::execute_ready_executables(
  refresh_wait_set(timeout);
   // Execute all the ready subscriptions
   for (size_t i = 0; i < wait_set_.size_of_subscriptions; ++i) {
-    if (wait_set_.size_of_subscriptions && i < exec_list.number_of_subscriptions) {
+    if (i < exec_list.number_of_subscriptions) {
       if (wait_set_.subscriptions[i]) {
         execute_subscription(exec_list.subscription[i]);
       }
@@ -225,7 +225,7 @@ StaticSingleThreadedExecutor::execute_ready_executables(
   }
   // Execute all the ready timers
   for (size_t i = 0; i < wait_set_.size_of_timers; ++i) {
-    if (wait_set_.size_of_timers && i < exec_list.number_of_timers) {
+    if (i < exec_list.number_of_timers) {
       if (wait_set_.timers[i] && exec_list.timer[i]->is_ready()) {
         execute_timer(exec_list.timer[i]);
       }
@@ -233,7 +233,7 @@ StaticSingleThreadedExecutor::execute_ready_executables(
   }
   // Execute all the ready services
   for (size_t i = 0; i < wait_set_.size_of_services; ++i) {
-    if (wait_set_.size_of_services && i < exec_list.number_of_services) {
+    if (i < exec_list.number_of_services) {
       if (wait_set_.services[i]) {
         execute_service(exec_list.service[i]);
       }
@@ -241,7 +241,7 @@ StaticSingleThreadedExecutor::execute_ready_executables(
   }
   // Execute all the ready clients
   for (size_t i = 0; i < wait_set_.size_of_clients; ++i) {
-    if (wait_set_.size_of_clients && i < exec_list.number_of_clients) {
+    if (i < exec_list.number_of_clients) {
       if (wait_set_.clients[i]) {
         execute_client(exec_list.client[i]);
       }
@@ -249,7 +249,7 @@ StaticSingleThreadedExecutor::execute_ready_executables(
   }
   // Execute all the ready waitables
   for (size_t i = 0; i < exec_list.number_of_waitables; ++i) {
-    if (exec_list.number_of_waitables && exec_list.waitable[i]->is_ready(&wait_set_)) {
+    if (exec_list.waitable[i]->is_ready(&wait_set_)) {
       exec_list.waitable[i]->execute();
     }
   }
