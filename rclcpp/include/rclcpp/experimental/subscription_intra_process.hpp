87fa896e3826ca75189e2ccf4aa07a68c51b5fdb
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-03-05 11:03:19 -0800

    Add ifdefs to remove tracing-related calls if tracing is disabled (#1001)
    * Add ifdefs to remove tracing-related calls if tracing is disabled
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>
    
    * Move ifndefs inside register_callback_for_tracing
    
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- rclcpp/include/rclcpp/experimental/subscription_intra_process.hpp
+++ rclcpp/include/rclcpp/experimental/subscription_intra_process.hpp
@@ -97,7 +97,9 @@ public:
     // The callback object gets copied, so if registration is done too early/before this point
     // (e.g. in `AnySubscriptionCallback::set()`), its address won't match any address used later
     // in subsequent tracepoints.
+#ifndef TRACETOOLS_DISABLED
     any_callback_.register_callback_for_tracing();
+#endif
   }
 
   bool
