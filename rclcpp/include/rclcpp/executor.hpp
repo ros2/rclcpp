79e36fdb33945b322226be72dc29c48f35b434cc
Donghee Ye <donghee.ye@samsung.com>
2020-03-23 11:28:50 +0900

    Apply comment: add spin_once_impl private method
    Signed-off-by: Donghee Ye <donghee.ye@samsung.com>

--- rclcpp/include/rclcpp/executor.hpp
+++ rclcpp/include/rclcpp/executor.hpp
@@ -219,10 +219,8 @@ public:
     RCLCPP_SCOPE_EXIT(this->spinning.store(false); );
     while (rclcpp::ok(this->context_) && spinning.load()) {
       // Do one item of work.
-      AnyExecutable any_exec;
-      if (get_next_executable(any_exec, timeout_left)) {
-        execute_any_executable(any_exec);
-      }
+      spin_once_impl(timeout_left);
+
       // Check if the future is set, return SUCCESS if it is.
       status = future.wait_for(std::chrono::seconds(0));
       if (status == std::future_status::ready) {
@@ -343,6 +341,9 @@ protected:
   std::shared_ptr<rclcpp::Context> context_;
 
   RCLCPP_DISABLE_COPY(Executor)
+
+  void
+  spin_once_impl(std::chrono::nanoseconds timeout);
 
   std::list<rclcpp::node_interfaces::NodeBaseInterface::WeakPtr> weak_nodes_;
   std::list<const rcl_guard_condition_t *> guard_conditions_;
