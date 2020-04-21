9e04e9f43068a2a1d9bb0d587fdf216c06bb70c7
Prajakta Gokhale <prajaktg@amazon.com>
2020-04-20 19:45:18 -0700

    Remove unnecessary if statement
    Signed-off-by: Prajakta Gokhale <prajaktg@amazon.com>

--- rclcpp/include/rclcpp/node_impl.hpp
+++ rclcpp/include/rclcpp/node_impl.hpp
@@ -92,11 +92,6 @@ Node::create_subscription(
   const SubscriptionOptionsWithAllocator<AllocatorT> & options,
   typename MessageMemoryStrategyT::SharedPtr msg_mem_strat)
 {
-  // Setup topic statistics collector if enabled.
-  if (rclcpp::detail::resolve_enable_topic_statistics(options, *node_base_)) {
-    // TODO(@dabonnie, @prajakta-gokhale): https://github.com/ros2/ros2/issues/901.
-  }
-
   return rclcpp::create_subscription<MessageT>(
     *this,
     extend_name_with_sub_namespace(topic_name, this->get_sub_namespace()),
