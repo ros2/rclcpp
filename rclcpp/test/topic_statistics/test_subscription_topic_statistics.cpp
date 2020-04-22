bb91b6c2ef54bac217090f14d3eb9aa0f79435d0
Devin Bonnie <47613035+dabonnie@users.noreply.github.com>
2020-04-22 16:09:41 -0700

    Integrate topic statistics (#1072)
    * Add SubscriberTopicStatistics class
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Add SubscriberTopicStatistics Test
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Address review comments
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Modify constructor to allow a node to create necessary components
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Fix docstring style
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Remove SetPublisherTimer method
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Change naming style to match rclcpp
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Address style issues
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Use rclcpp:Time
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Address review comments
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Remove unnecessary check for null publisher timer
    Move anonymous namespace function to private class method
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Update message dependency
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Initial integration of Subscriber Topic Statistics
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Fix nanoseconds used for Topic Stats
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Add simple publishing test
    Minor fixes
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Add test utils header
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Integrate with Topic Statistics options
    Fixes after rebasing with master
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Update after rebasing
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Address minor review comments
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Move Topic Statistics instantiation to create_subscription
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Fix rebase issue
    Fix topic statistics enable flag usage
    Address minor formatting
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Move new timer creation method to relevant header
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Add timers interface to topic interface
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Use new create timer method
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>
    
    * Address review comments
    
    Signed-off-by: Devin Bonnie <dbbonnie@amazon.com>

--- rclcpp/test/topic_statistics/test_subscription_topic_statistics.cpp
+++ rclcpp/test/topic_statistics/test_subscription_topic_statistics.cpp
@@ -14,6 +14,9 @@
 
 #include <gtest/gtest.h>
 
+#include <atomic>
+#include <chrono>
+#include <iostream>
 #include <memory>
 #include <string>
 #include <vector>
@@ -24,23 +27,34 @@
 #include "rclcpp/node.hpp"
 #include "rclcpp/qos.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription_options.hpp"
 
 #include "rclcpp/topic_statistics/subscription_topic_statistics.hpp"
 
 #include "statistics_msgs/msg/metrics_message.hpp"
+#include "statistics_msgs/msg/statistic_data_point.hpp"
+#include "statistics_msgs/msg/statistic_data_type.hpp"
+
 #include "test_msgs/msg/empty.hpp"
+
+#include "test_topic_stats_utils.hpp"
 
 namespace
 {
-constexpr const char kTestNodeName[]{"test_sub_stats_node"};
+constexpr const char kTestPubNodeName[]{"test_pub_stats_node"};
+constexpr const char kTestSubNodeName[]{"test_sub_stats_node"};
 constexpr const char kTestSubStatsTopic[]{"/test_sub_stats_topic"};
 constexpr const char kTestTopicStatisticsTopic[]{"/test_topic_statistics_topic"};
 constexpr const uint64_t kNoSamples{0};
+constexpr const std::chrono::milliseconds kTestStatsPublishPeriod{5000};
+constexpr const std::chrono::seconds kTestDuration{10};
 }  // namespace
 
 using test_msgs::msg::Empty;
-using statistics_msgs::msg::MetricsMessage;
 using rclcpp::topic_statistics::SubscriptionTopicStatistics;
+using statistics_msgs::msg::MetricsMessage;
+using statistics_msgs::msg::StatisticDataPoint;
+using statistics_msgs::msg::StatisticDataType;
 using libstatistics_collector::moving_average_statistics::StatisticData;
 
 template<typename CallbackMessageT>
@@ -64,6 +78,44 @@ public:
 };
 
 /**
+ * Empty publisher node: used to publish empty messages
+ */
+class EmptyPublisher : public rclcpp::Node
+{
+public:
+  EmptyPublisher(
+    const std::string & name, const std::string & topic,
+    const std::chrono::milliseconds & publish_period = std::chrono::milliseconds{100})
+  : Node(name)
+  {
+    publisher_ = create_publisher<Empty>(topic, 10);
+    publish_timer_ = this->create_wall_timer(
+      publish_period, [this]() {
+        this->publish_message();
+      });
+  }
+
+  virtual ~EmptyPublisher() = default;
+
+  size_t get_number_published()
+  {
+    return number_published_.load();
+  }
+
+private:
+  void publish_message()
+  {
+    ++number_published_;
+    auto msg = Empty{};
+    publisher_->publish(msg);
+  }
+
+  rclcpp::Publisher<Empty>::SharedPtr publisher_;
+  std::atomic<size_t> number_published_{0};
+  rclcpp::TimerBase::SharedPtr publish_timer_;
+};
+
+/**
  * Empty subscriber node: used to create subscriber topic statistics requirements
  */
 class EmptySubscriber : public rclcpp::Node
@@ -72,6 +124,10 @@ public:
   EmptySubscriber(const std::string & name, const std::string & topic)
   : Node(name)
   {
+    // manually enable topic statistics via options
+    auto options = rclcpp::SubscriptionOptions();
+    options.topic_stats_options.state = rclcpp::TopicStatisticsState::Enable;
+
     auto callback = [this](Empty::UniquePtr msg) {
         this->receive_message(*msg);
       };
@@ -79,16 +135,15 @@ public:
         std::function<void(Empty::UniquePtr)>>(
       topic,
       rclcpp::QoS(rclcpp::KeepAll()),
-      callback);
+      callback,
+      options);
   }
-
   virtual ~EmptySubscriber() = default;
 
 private:
   void receive_message(const Empty &) const
   {
   }
-
   rclcpp::Subscription<Empty>::SharedPtr subscription_;
 };
 
@@ -102,7 +157,7 @@ protected:
   {
     rclcpp::init(0 /* argc */, nullptr /* argv */);
     empty_subscriber = std::make_shared<EmptySubscriber>(
-      kTestNodeName,
+      kTestSubNodeName,
       kTestSubStatsTopic);
   }
 
@@ -122,12 +177,10 @@ TEST_F(TestSubscriptionTopicStatisticsFixture, test_manual_construction)
     kTestTopicStatisticsTopic,
     10);
 
-  // construct the instance
+  // construct a separate instance
   auto sub_topic_stats = std::make_unique<TestSubscriptionTopicStatistics<Empty>>(
     empty_subscriber->get_name(),
     topic_stats_publisher);
-
-  using libstatistics_collector::moving_average_statistics::StatisticData;
 
   // expect no data has been collected / no samples received
   for (const auto & data : sub_topic_stats->get_current_collector_data()) {
@@ -136,5 +189,59 @@ TEST_F(TestSubscriptionTopicStatisticsFixture, test_manual_construction)
     EXPECT_TRUE(std::isnan(data.max));
     EXPECT_TRUE(std::isnan(data.standard_deviation));
     EXPECT_EQ(kNoSamples, data.sample_count);
+  }
+}
+
+TEST_F(TestSubscriptionTopicStatisticsFixture, test_receive_single_empty_stats_message)
+{
+  // create an empty publisher
+  auto empty_publisher = std::make_shared<EmptyPublisher>(
+    kTestPubNodeName,
+    kTestSubStatsTopic);
+  // empty_subscriber has a topic statistics instance as part of its subscription
+  // this will listen to and generate statistics for the empty message
+
+  // create a listener for topic statistics messages
+  auto statistics_listener = std::make_shared<rclcpp::topic_statistics::MetricsMessageSubscriber>(
+    "test_receive_single_empty_stats_message_listener",
+    "/statistics");
+
+  rclcpp::executors::SingleThreadedExecutor ex;
+  ex.add_node(empty_publisher);
+  ex.add_node(statistics_listener);
+  ex.add_node(empty_subscriber);
+
+  // spin and get future
+  ex.spin_until_future_complete(
+    statistics_listener->GetFuture(),
+    kTestDuration);
+
+  // compare message counts, sample count should be the same as published and received count
+  EXPECT_EQ(1, statistics_listener->GetNumberOfMessagesReceived());
+
+  // check the received message and the data types
+  const auto received_message = statistics_listener->GetLastReceivedMessage();
+  for (const auto & stats_point : received_message.statistics) {
+    const auto type = stats_point.data_type;
+    switch (type) {
+      case StatisticDataType::STATISTICS_DATA_TYPE_SAMPLE_COUNT:
+        EXPECT_LT(0, stats_point.data) << "unexpected sample count";
+        break;
+      case StatisticDataType::STATISTICS_DATA_TYPE_AVERAGE:
+        EXPECT_LT(0, stats_point.data) << "unexpected avg";
+        break;
+      case StatisticDataType::STATISTICS_DATA_TYPE_MINIMUM:
+        EXPECT_LT(0, stats_point.data) << "unexpected min";
+        break;
+      case StatisticDataType::STATISTICS_DATA_TYPE_MAXIMUM:
+        EXPECT_LT(0, stats_point.data) << "unexpected max";
+        break;
+      case StatisticDataType::STATISTICS_DATA_TYPE_STDDEV:
+        EXPECT_LT(0, stats_point.data) << "unexpected stddev";
+        break;
+      default:
+        FAIL() << "received unknown statistics type: " << std::dec <<
+          static_cast<unsigned int>(type);
+    }
   }
 }
