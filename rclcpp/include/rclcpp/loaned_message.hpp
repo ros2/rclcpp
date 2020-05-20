4efcfdc16bd039326896f9e38bc06255c6268797
Chris Lalancette <clalancette@openrobotics.org>
2020-05-20 19:54:18 +0000

    Make the rcl publisher handle a shared pointer.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rclcpp/include/rclcpp/loaned_message.hpp
+++ rclcpp/include/rclcpp/loaned_message.hpp
@@ -66,7 +66,7 @@ public:
     if (pub_.can_loan_messages()) {
       void * message_ptr = nullptr;
       auto ret = rcl_borrow_loaned_message(
-        pub_.get_publisher_handle(),
+        pub_.get_publisher_handle().get(),
         rosidl_typesupport_cpp::get_message_type_support_handle<MessageT>(),
         &message_ptr);
       if (RCL_RET_OK != ret) {
@@ -139,7 +139,7 @@ public:
     if (pub_.can_loan_messages()) {
       // return allocated memory to the middleware
       auto ret =
-        rcl_return_loaned_message_from_publisher(pub_.get_publisher_handle(), message_);
+        rcl_return_loaned_message_from_publisher(pub_.get_publisher_handle().get(), message_);
       if (ret != RCL_RET_OK) {
         RCLCPP_ERROR(
           error_logger, "rcl_deallocate_loaned_message failed: %s", rcl_get_error_string().str);
