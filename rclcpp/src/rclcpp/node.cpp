0ef9731feb5d2392dca7467596dff95b8747a60b
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2020-05-26 10:26:51 -0700

    expose get_service_names_and_types_by_node from rcl in rclcpp (#1131)
    * expose get_service_names_and_types_by_node from rcl in rclcpp
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * fix spelling
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * zero initialize
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * check return value and cleanup
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * use throw_from_rcl_error
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>
    
    * cleanup error handling
    
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- rclcpp/src/rclcpp/node.cpp
+++ rclcpp/src/rclcpp/node.cpp
@@ -352,6 +352,15 @@ Node::get_service_names_and_types() const
   return node_graph_->get_service_names_and_types();
 }
 
+std::map<std::string, std::vector<std::string>>
+Node::get_service_names_and_types_by_node(
+  const std::string & node_name,
+  const std::string & namespace_) const
+{
+  return node_graph_->get_service_names_and_types_by_node(
+    node_name, namespace_);
+}
+
 size_t
 Node::count_publishers(const std::string & topic_name) const
 {
