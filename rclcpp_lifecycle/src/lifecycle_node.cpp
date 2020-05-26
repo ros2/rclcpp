63a48a19988d31c669462abc4178ce010278d409
claireyywang <22240514+claireyywang@users.noreply.github.com>
2020-05-26 13:28:25 -0700

    remove return from void fn
    Signed-off-by: claireyywang <22240514+claireyywang@users.noreply.github.com>

--- rclcpp_lifecycle/src/lifecycle_node.cpp
+++ rclcpp_lifecycle/src/lifecycle_node.cpp
@@ -264,7 +264,7 @@ LifecycleNode::add_on_set_parameters_callback(OnParametersSetCallbackType callba
 void
 LifecycleNode::remove_on_set_parameters_callback(const OnSetParametersCallbackHandle * const callback)
 {
-  return node_parameters_->remove_on_set_parameters_callback(callback);
+  node_parameters_->remove_on_set_parameters_callback(callback);
 }
 
 rclcpp::Node::OnParametersSetCallbackType
