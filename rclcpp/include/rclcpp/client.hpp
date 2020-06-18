101c26dd95bad7f7f3ac865653e9a0b3d954fd27
Tomoya.Fujita <Tomoya.Fujita@sony.com>
2020-06-18 23:15:17 +0900

    callback should be perfectly-forwarded.
    Signed-off-by: Tomoya.Fujita <Tomoya.Fujita@sony.com>

--- rclcpp/include/rclcpp/client.hpp
+++ rclcpp/include/rclcpp/client.hpp
@@ -361,7 +361,8 @@ public:
     SharedPromiseWithRequest promise = std::make_shared<PromiseWithRequest>();
     SharedFutureWithRequest future_with_request(promise->get_future());
 
-    auto wrapping_cb = [future_with_request, promise, request, &cb](SharedFuture future) {
+    auto wrapping_cb = [future_with_request, promise, request,
+                        cb=std::forward<CallbackWithRequestType>(cb)](SharedFuture future) {
         auto response = future.get();
         promise->set_value(std::make_pair(request, response));
         cb(future_with_request);
