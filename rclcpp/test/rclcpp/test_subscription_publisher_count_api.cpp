46e4a1f7457f2bd1da2b2c6128e1c6995ce1fc1d
Michel Hidalgo <michel@ekumenlabs.com>
2020-06-26 20:00:14 -0300

    Fix pub/sub count API tests. (#1203)
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- rclcpp/test/rclcpp/test_subscription_publisher_count_api.cpp
+++ rclcpp/test/rclcpp/test_subscription_publisher_count_api.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019 Open Source Robotics Foundation, Inc.
+// Copyright 2019-2020 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,21 +23,79 @@
 
 #include "test_msgs/msg/empty.hpp"
 
-using test_msgs::msg::Empty;
-
-struct TestParameters
+namespace
 {
-  rclcpp::NodeOptions node_options;
-  std::string description;
+
+template<typename ... Ts>
+class NodeCreationPolicy
+{
+public:
+  rclcpp::NodeOptions & node_options()
+  {
+    return options_;
+  }
+
+private:
+  rclcpp::NodeOptions options_;
 };
 
-std::ostream & operator<<(std::ostream & out, const TestParameters & params)
+template<typename T, typename ... Ts>
+class NodeCreationPolicy<T, Ts...>
 {
-  out << params.description;
-  return out;
-}
+public:
+  NodeCreationPolicy()
+  {
+    gather<T, Ts...>(options_);
+  }
 
-class TestSubscriptionPublisherCount : public ::testing::TestWithParam<TestParameters>
+  rclcpp::NodeOptions & node_options()
+  {
+    return options_;
+  }
+
+private:
+  template<typename U>
+  static rclcpp::NodeOptions &
+  gather(rclcpp::NodeOptions & options)
+  {
+    return U::gather(options);
+  }
+
+  template<typename U, typename V, typename ... Ws>
+  static rclcpp::NodeOptions &
+  gather(rclcpp::NodeOptions & options)
+  {
+    return gather<V, Ws...>(U::gather(options));
+  }
+
+  rclcpp::NodeOptions options_;
+};
+
+struct UseCustomContext
+{
+  static rclcpp::NodeOptions & gather(rclcpp::NodeOptions & options)
+  {
+    auto context = rclcpp::Context::make_shared();
+    context->init(0, nullptr);
+    return options.context(context);
+  }
+};
+
+struct PrintTestDescription
+{
+  template<typename T>
+  static std::string GetName(int i)
+  {
+    static_cast<void>(i);
+    return T::description;
+  }
+};
+
+}  // namespace
+
+
+template<typename TestDescription>
+class TestSubscriptionPublisherCount : public ::testing::Test
 {
 public:
   static void SetUpTestCase()
@@ -50,78 +108,59 @@ public:
   }
 
 protected:
-  void SetUp() {}
-  void TearDown() {}
-  static std::chrono::milliseconds offset;
+  static void OnMessage(const test_msgs::msg::Empty::SharedPtr msg)
+  {
+    (void)msg;
+  }
+
+  std::chrono::milliseconds offset{2000};
 };
 
-std::chrono::milliseconds TestSubscriptionPublisherCount::offset = std::chrono::milliseconds(2000);
-
-void OnMessage(const test_msgs::msg::Empty::SharedPtr msg)
+struct OneContextPerTest
 {
-  (void)msg;
-}
+  static constexpr const char * description = "one_context_test";
+  using NodeCreationPolicy = ::NodeCreationPolicy<>;
+};
 
-TEST_P(TestSubscriptionPublisherCount, increasing_and_decreasing_counts)
+struct TwoContextsPerTest
 {
-  rclcpp::NodeOptions node_options = GetParam().node_options;
-  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>(
-    "my_node",
-    "/ns",
-    node_options);
-  auto subscription = node->create_subscription<Empty>("/topic", 10, &OnMessage);
+  static constexpr const char * description = "two_contexts_test";
+  using NodeCreationPolicy = ::NodeCreationPolicy<UseCustomContext>;
+};
+
+using AllTestDescriptions = ::testing::Types<OneContextPerTest, TwoContextsPerTest>;
+TYPED_TEST_CASE(TestSubscriptionPublisherCount, AllTestDescriptions, PrintTestDescription);
+
+
+using test_msgs::msg::Empty;
+
+TYPED_TEST(TestSubscriptionPublisherCount, increasing_and_decreasing_counts)
+{
+  using TestDescription = TypeParam;
+  rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("my_node", "/ns");
+  auto subscription = node->create_subscription<Empty>(
+    "/topic", 10, &TestSubscriptionPublisherCount<TestDescription>::OnMessage);
 
   EXPECT_EQ(subscription->get_publisher_count(), 0u);
   {
     auto pub = node->create_publisher<Empty>("/topic", 10);
-    rclcpp::sleep_for(offset);
+    rclcpp::sleep_for(this->offset);
     EXPECT_EQ(subscription->get_publisher_count(), 1u);
     {
+      typename TestDescription::NodeCreationPolicy node_creation_policy;
       rclcpp::Node::SharedPtr another_node = std::make_shared<rclcpp::Node>(
         "another_node",
         "/ns",
-        node_options);
+        node_creation_policy.node_options());
       auto another_pub =
         another_node->create_publisher<Empty>("/topic", 10);
 
-      rclcpp::sleep_for(offset);
+      rclcpp::sleep_for(this->offset);
       EXPECT_EQ(subscription->get_publisher_count(), 2u);
     }
-    rclcpp::sleep_for(offset);
+    rclcpp::sleep_for(this->offset);
     EXPECT_EQ(subscription->get_publisher_count(), 1u);
   }
-  rclcpp::sleep_for(offset);
+  rclcpp::sleep_for(this->offset);
   EXPECT_EQ(subscription->get_publisher_count(), 0u);
 }
-
-auto get_new_context()
-{
-  auto context = rclcpp::Context::make_shared();
-  context->init(0, nullptr);
-  return context;
-}
-
-TestParameters parameters[] = {
-  /*
-     Testing subscription publisher count api.
-     One context.
-   */
-  {
-    rclcpp::NodeOptions(),
-    "one_context_test"
-  },
-  /*
-     Testing subscription publisher count api.
-     Two contexts.
-   */
-  {
-    rclcpp::NodeOptions().context(get_new_context()),
-    "two_contexts_test"
-  }
-};
-
-INSTANTIATE_TEST_CASE_P(
-  TestWithDifferentNodeOptions,
-  TestSubscriptionPublisherCount,
-  testing::ValuesIn(parameters),
-  testing::PrintToStringParamName());
