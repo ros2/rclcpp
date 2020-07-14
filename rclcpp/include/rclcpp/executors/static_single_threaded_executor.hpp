b22574305c7d4a013e5609618cf7c87e8edaa7d3
brawner <brawner@gmail.com>
2020-07-14 09:53:51 -0700

    Derive and throw exception in spin_some spin_all for StaticSingleThreadedExecutor (#1220)
    * Derive and throw exception in spin_some spin_all
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Fix style and add unit test
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- rclcpp/include/rclcpp/executors/static_single_threaded_executor.hpp
+++ rclcpp/include/rclcpp/executors/static_single_threaded_executor.hpp
@@ -190,6 +190,38 @@ public:
     return rclcpp::FutureReturnCode::INTERRUPTED;
   }
 
+  /// Not yet implemented, see https://github.com/ros2/rclcpp/issues/1219 for tracking
+  RCLCPP_PUBLIC
+  void
+  spin_some(std::chrono::nanoseconds max_duration = std::chrono::nanoseconds(0)) override
+  {
+    (void)max_duration;
+    throw rclcpp::exceptions::UnimplementedError(
+            "spin_some is not implemented for StaticSingleThreadedExecutor, use spin or "
+            "spin_until_future_complete");
+  }
+
+  /// Not yet implemented, see https://github.com/ros2/rclcpp/issues/1219 for tracking
+  RCLCPP_PUBLIC
+  void
+  spin_all(std::chrono::nanoseconds) override
+  {
+    throw rclcpp::exceptions::UnimplementedError(
+            "spin_all is not implemented for StaticSingleThreadedExecutor, use spin or "
+            "spin_until_future_complete");
+  }
+
+  /// Not yet implemented, see https://github.com/ros2/rclcpp/issues/1219 for tracking
+  RCLCPP_PUBLIC
+  void
+  spin_once(std::chrono::nanoseconds timeout = std::chrono::nanoseconds(-1)) override
+  {
+    (void)timeout;
+    throw rclcpp::exceptions::UnimplementedError(
+            "spin_once is not implemented for StaticSingleThreadedExecutor, use spin or "
+            "spin_until_future_complete");
+  }
+
 protected:
   /// Check which executables in ExecutableList struct are ready from wait_set and execute them.
   /**
