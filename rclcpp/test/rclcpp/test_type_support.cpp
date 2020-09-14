7add35f680eaaa2299633eea22b6397fd2918775
Jorge Perez <jjperez@ekumenlabs.com>
2020-09-14 15:01:01 -0300

    Fix uncrustify
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- rclcpp/test/rclcpp/test_type_support.cpp
+++ rclcpp/test/rclcpp/test_type_support.cpp
@@ -44,12 +44,12 @@ public:
       &service_handle, node_handle, ts, "base_node_service", &service_options);
     if (ret != RCL_RET_OK) {
       return ::testing::AssertionFailure() <<
-        "Failed rcl_service_init with error string: " << rcl_get_error_string().str;
+             "Failed rcl_service_init with error string: " << rcl_get_error_string().str;
     }
     ret = rcl_service_fini(&service_handle, node_handle);
     if (ret != RCL_RET_OK) {
       return ::testing::AssertionFailure() <<
-        "Failed rcl_service_fini with error string: " << rcl_get_error_string().str;
+             "Failed rcl_service_fini with error string: " << rcl_get_error_string().str;
     }
     return ::testing::AssertionSuccess();
   }
