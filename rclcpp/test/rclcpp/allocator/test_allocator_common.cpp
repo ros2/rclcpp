46da8348a880d8701f66ee49eb06169b0064f8fc
Steve Wolter <swolter@google.com>
2020-09-22 08:18:37 +0000

    Make get_rcl_allocator a function family
    As explained in #1254, there's conceptually no way to implement RCL
    allocators in terms of C++ allocators. In order to fix this behavior,
    we have to delete the generic version of get_rcl_allocator. Since some
    ROS code depends on this, we need to allow users to write their own
    version of get_rcl_allocator for allocators that support the C-style
    interface (most do).
    
    So this CL changes get_rcl_allocator from a template function
    into a family of (potentially templated) functions, which allows
    users to add their own overloads and rely on the "most specialized"
    mechanism for function specialization to select the right one. See
    http://www.gotw.ca/publications/mill17.htm for details. This also
    allows us to return get_rcl_default_allocator for all specializations
    of std::allocator (previously, only for std::allocator), which will
    already fix #1254 for pretty much all clients. I'll continue to work
    on deleting the generic version, though, to make sure that nobody is
    accidentally bitten by it.
    
    I've tried to test this by doing a full ROS compilation following the
    Dockerfile of the source Docker image, and all packages compile.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Addressed code style test failures.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Update comments.
    
    Incidentally, this also reruns the flaky test suite, which
    seems to be using the real DDS middleware and to be prone
    to cross-talk.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Also update recently added tests.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Added reference to bug number.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Extend allocator lifetime in options.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Drop the generic version of get_rcl_allocator.
    
    This allows us to simplify a bunch of code as well.
    
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Update rclcpp/include/rclcpp/allocator/allocator_common.hpp
    
    Co-authored-by: William Woodall <william+github@osrfoundation.org>
    Signed-off-by: Steve Wolter <swolter@google.com>
    
    Revert accidental deletion of allocator creation.
    
    Signed-off-by: Steve Wolter <swolter@google.com>

--- rclcpp/test/rclcpp/allocator/test_allocator_common.cpp
+++ rclcpp/test/rclcpp/allocator/test_allocator_common.cpp
@@ -18,54 +18,17 @@
 
 #include "rclcpp/allocator/allocator_common.hpp"
 
-TEST(TestAllocatorCommon, retyped_allocate) {
-  std::allocator<int> allocator;
-  void * untyped_allocator = &allocator;
-  void * allocated_mem =
-    rclcpp::allocator::retyped_allocate<std::allocator<char>>(1u, untyped_allocator);
-  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
-  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
-  ASSERT_TRUE(nullptr != allocated_mem);
-
-  auto code = [&untyped_allocator, allocated_mem]() {
-      rclcpp::allocator::retyped_deallocate<int, std::allocator<int>>(
-        allocated_mem, untyped_allocator);
-    };
-  EXPECT_NO_THROW(code());
-
-  allocated_mem = allocator.allocate(1);
-  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
-  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
-  ASSERT_TRUE(nullptr != allocated_mem);
-  void * reallocated_mem =
-    rclcpp::allocator::retyped_reallocate<int, std::allocator<int>>(
-    allocated_mem, 2u, untyped_allocator);
-  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
-  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
-  ASSERT_TRUE(nullptr != reallocated_mem);
-
-  auto code2 = [&untyped_allocator, reallocated_mem]() {
-      rclcpp::allocator::retyped_deallocate<int, std::allocator<int>>(
-        reallocated_mem, untyped_allocator);
-    };
-  EXPECT_NO_THROW(code2());
-}
-
-TEST(TestAllocatorCommon, retyped_zero_allocate_basic) {
+TEST(TestAllocatorCommon, retyped_zero_allocate_basic)
+{
   std::allocator<int> allocator;
   void * untyped_allocator = &allocator;
   void * allocated_mem =
     rclcpp::allocator::retyped_zero_allocate<std::allocator<char>>(20u, 1u, untyped_allocator);
   ASSERT_TRUE(nullptr != allocated_mem);
-
-  auto code = [&untyped_allocator, allocated_mem]() {
-      rclcpp::allocator::retyped_deallocate<char, std::allocator<char>>(
-        allocated_mem, untyped_allocator);
-    };
-  EXPECT_NO_THROW(code());
 }
 
-TEST(TestAllocatorCommon, retyped_zero_allocate) {
+TEST(TestAllocatorCommon, retyped_zero_allocate)
+{
   std::allocator<int> allocator;
   void * untyped_allocator = &allocator;
   void * allocated_mem =
@@ -73,34 +36,12 @@ TEST(TestAllocatorCommon, retyped_zero_allocate) {
   // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
   // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
   ASSERT_TRUE(nullptr != allocated_mem);
-
-  auto code = [&untyped_allocator, allocated_mem]() {
-      rclcpp::allocator::retyped_deallocate<int, std::allocator<int>>(
-        allocated_mem, untyped_allocator);
-    };
-  EXPECT_NO_THROW(code());
-
-  allocated_mem = allocator.allocate(1);
-  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
-  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
-  ASSERT_TRUE(nullptr != allocated_mem);
-  void * reallocated_mem =
-    rclcpp::allocator::retyped_reallocate<int, std::allocator<int>>(
-    allocated_mem, 2u, untyped_allocator);
-  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
-  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
-  ASSERT_TRUE(nullptr != reallocated_mem);
-
-  auto code2 = [&untyped_allocator, reallocated_mem]() {
-      rclcpp::allocator::retyped_deallocate<int, std::allocator<int>>(
-        reallocated_mem, untyped_allocator);
-    };
-  EXPECT_NO_THROW(code2());
 }
 
-TEST(TestAllocatorCommon, get_rcl_allocator) {
+TEST(TestAllocatorCommon, get_rcl_allocator)
+{
   std::allocator<int> allocator;
-  auto rcl_allocator = rclcpp::allocator::get_rcl_allocator<int>(allocator);
+  auto rcl_allocator = rclcpp::allocator::get_rcl_allocator(allocator);
   EXPECT_NE(nullptr, rcl_allocator.allocate);
   EXPECT_NE(nullptr, rcl_allocator.deallocate);
   EXPECT_NE(nullptr, rcl_allocator.reallocate);
@@ -108,10 +49,10 @@ TEST(TestAllocatorCommon, get_rcl_allocator) {
   // Not testing state as that may or may not be null depending on platform
 }
 
-TEST(TestAllocatorCommon, get_void_rcl_allocator) {
+TEST(TestAllocatorCommon, get_void_rcl_allocator)
+{
   std::allocator<void> allocator;
-  auto rcl_allocator =
-    rclcpp::allocator::get_rcl_allocator<void, std::allocator<void>>(allocator);
+  auto rcl_allocator = rclcpp::allocator::get_rcl_allocator(allocator);
   EXPECT_NE(nullptr, rcl_allocator.allocate);
   EXPECT_NE(nullptr, rcl_allocator.deallocate);
   EXPECT_NE(nullptr, rcl_allocator.reallocate);
