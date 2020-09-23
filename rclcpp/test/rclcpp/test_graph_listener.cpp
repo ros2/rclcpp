ee7c758a6dcc7306405b5bdecc05b354fd592513
Jorge Perez <jjperez@ekumenlabs.com>
2020-09-23 13:04:14 -0300

    Add tests start graph listener
    Signed-off-by: Jorge Perez <jjperez@ekumenlabs.com>

--- rclcpp/test/rclcpp/test_graph_listener.cpp
+++ rclcpp/test/rclcpp/test_graph_listener.cpp
@@ -18,11 +18,15 @@
 #include "rclcpp/graph_listener.hpp"
 #include "rclcpp/rclcpp.hpp"
 
+#include "../mocking_utils/patch.hpp"
+#include "../utils/rclcpp_gtest_macros.hpp"
+
 namespace
 {
 
 constexpr char node_name[] = "node";
 constexpr char node_namespace[] = "ns";
+constexpr char shutdown_error_str[] = "GraphListener already shutdown";
 
 }  // namespace
 
@@ -53,3 +57,56 @@ private:
   std::shared_ptr<rclcpp::graph_listener::GraphListener> graph_listener_;
 };
 
+/* Run base class init/shutdown */
+TEST_F(TestGraphListener, construction_and_destruction) {
+  void();
+}
+
+// Required for mocking_utils below
+MOCKING_UTILS_BOOL_OPERATOR_RETURNS_FALSE(rcl_guard_condition_options_t, ==)
+MOCKING_UTILS_BOOL_OPERATOR_RETURNS_FALSE(rcl_guard_condition_options_t, !=)
+MOCKING_UTILS_BOOL_OPERATOR_RETURNS_FALSE(rcl_guard_condition_options_t, <)
+MOCKING_UTILS_BOOL_OPERATOR_RETURNS_FALSE(rcl_guard_condition_options_t, >)
+
+/* Error creating a new graph listener */
+TEST_F(TestGraphListener, error_construct_graph_listener) {
+  using rclcpp::contexts::get_global_default_context;
+  auto mock = mocking_utils::patch_and_return(
+    "lib:rclcpp", rcl_guard_condition_init, RCL_RET_ERROR);
+
+  using rclcpp::contexts::get_global_default_context;
+  RCLCPP_EXPECT_THROW_EQ({
+  auto graph_listener_error =
+    std::make_shared<rclcpp::graph_listener::GraphListener>(get_global_default_context());
+    }, std::runtime_error("failed to create interrupt guard condition: error not set"));
+}
+
+/* Errors that occur when initializing the graph_listener */
+TEST_F(TestGraphListener, error_start_graph_listener) {
+  {
+    // Invalid incomplete type mock, skip
+    /*
+    auto mock = mocking_utils::patch_and_return(
+      "lib:rclcpp", rcl_wait_set_init, RCL_RET_ERROR);
+    RCLCPP_EXPECT_THROW_EQ({
+      graph_listener()->start_if_not_started();
+    }, std::runtime_error("failed to initialize wait set: error not set"));
+    */
+  }
+  {
+    auto context_to_destroy = std::make_shared<rclcpp::contexts::DefaultContext>();
+    context_to_destroy->init(0, nullptr);
+    auto graph_listener_error =
+      std::make_shared<rclcpp::graph_listener::GraphListener>(context_to_destroy);
+    context_to_destroy.reset();
+    RCLCPP_EXPECT_THROW_EQ({
+      graph_listener_error->start_if_not_started();
+    }, std::runtime_error("parent context was destroyed"));
+  }
+  {
+    EXPECT_NO_THROW(graph_listener()->shutdown());
+    RCLCPP_EXPECT_THROW_EQ({
+      graph_listener()->start_if_not_started();
+      }, std::runtime_error(shutdown_error_str));
+  }
+}
