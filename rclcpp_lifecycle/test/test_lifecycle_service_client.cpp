b5b0922d1731caf97387d95b712fce4c65e5e2aa
Stephen Brawner <brawner@gmail.com>
2020-10-12 14:37:00 -0700

    Increase test timeouts of slow running tests with rmw_connext_cpp
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

--- rclcpp_lifecycle/test/test_lifecycle_service_client.cpp
+++ rclcpp_lifecycle/test/test_lifecycle_service_client.cpp
@@ -52,6 +52,10 @@ constexpr char const * node_get_transition_graph_topic =
   "/lc_talker/get_transition_graph";
 const lifecycle_msgs::msg::State unknown_state = lifecycle_msgs::msg::State();
 
+// Note: This is a long running test with rmw_connext_cpp, if you change this file, please check
+// that this test can complete fully, or adjust the timeout as necessary.
+// See https://github.com/ros2/rmw_connext/issues/325 for resolution
+
 class EmptyLifecycleNode : public rclcpp_lifecycle::LifecycleNode
 {
 public:
