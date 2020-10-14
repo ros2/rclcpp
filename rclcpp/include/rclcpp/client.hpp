75635382c3a3d7fc0d048eae3a1468374d1701f0
iRobot ROS <49500531+irobot-ros@users.noreply.github.com>
2020-10-14 12:16:23 +0100

    Update client.hpp

--- rclcpp/include/rclcpp/client.hpp
+++ rclcpp/include/rclcpp/client.hpp
@@ -37,8 +37,8 @@
 #include "rclcpp/expand_topic_or_service_name.hpp"
 #include "rclcpp/visibility_control.hpp"
 
-#include "rcutils/logging_macros.h"
 #include "rcutils/executor_event_types.h"
+#include "rcutils/logging_macros.h"
 
 #include "rmw/error_handling.h"
 #include "rmw/rmw.h"
