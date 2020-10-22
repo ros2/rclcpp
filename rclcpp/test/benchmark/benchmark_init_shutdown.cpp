eb7c46ea43a8740fc398792869cc47a5b2d34299
brawner <brawner@gmail.com>
2020-10-22 15:04:13 -0700

    Initial benchmark tests for rclcpp::init/shutdown create/destroy node (#1411)
    * Initial benchmark tests for rclcpp::init/shutdown create/destroy node
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Pr feedback
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Fixes to cmakelists
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Remove quotes
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Move find_package calls
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Skip create/destroy node for rmw_connext_cpp
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * SKIP TEST in cmakelists
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Add warmup loops
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * remove for loop
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * reset_heap_counters
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>
    
    * Change to make_shared
    
    Signed-off-by: Stephen Brawner <brawner@gmail.com>

---
+++ rclcpp/test/benchmark/benchmark_init_shutdown.cpp
@@ -0,0 +1,53 @@
+// Copyright 2020 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "performance_test_fixture/performance_test_fixture.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+
+using performance_test_fixture::PerformanceTest;
+
+BENCHMARK_F(PerformanceTest, rclcpp_init)(benchmark::State & state)
+{
+  // Warmup and prime caches
+  rclcpp::init(0, nullptr);
+  rclcpp::shutdown();
+
+  reset_heap_counters();
+  for (auto _ : state) {
+    rclcpp::init(0, nullptr);
+
+    state.PauseTiming();
+    rclcpp::shutdown();
+    state.ResumeTiming();
+    benchmark::ClobberMemory();
+  }
+}
+
+BENCHMARK_F(PerformanceTest, rclcpp_shutdown)(benchmark::State & state)
+{
+  // Warmup and prime caches
+  rclcpp::init(0, nullptr);
+  rclcpp::shutdown();
+
+  reset_heap_counters();
+  for (auto _ : state) {
+    state.PauseTiming();
+    rclcpp::init(0, nullptr);
+    state.ResumeTiming();
+
+    rclcpp::shutdown();
+    benchmark::ClobberMemory();
+  }
+}
