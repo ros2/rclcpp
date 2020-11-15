886ed5d80ce947a362ce0d9f329bd23d73931d87
InitialDuan <runtaoduan@hotmail.com>
2020-11-15 16:22:52 +0100

    added comments
    Signed-off-by: InitialDuan <runtaoduan@hotmail.com>

--- rclcpp/include/rclcpp/create_subscription.hpp
+++ rclcpp/include/rclcpp/create_subscription.hpp
@@ -26,10 +26,10 @@
 
 #include "rclcpp/node_interfaces/get_node_timers_interface.hpp"
 #include "rclcpp/node_interfaces/get_node_topics_interface.hpp"
-#include "rclcpp/node_interfaces/get_node_parameters_interface.hpp"
+#include "rclcpp/node_interfaces/get_node_parameters_interface.hpp"	//added package
 #include "rclcpp/node_interfaces/node_timers_interface.hpp"
 #include "rclcpp/node_interfaces/node_topics_interface.hpp"
-#include "rclcpp/node_interfaces/node_parameters_interface.hpp"
+#include "rclcpp/node_interfaces/node_parameters_interface.hpp"	//added package
 
 #include "rclcpp/create_publisher.hpp"
 #include "rclcpp/create_timer.hpp"
@@ -101,63 +101,63 @@ create_subscription(
   using rclcpp::node_interfaces::get_node_parameters_interface;
   auto node_parameters = get_node_parameters_interface(std::forward<NodeT>(node));
   
-  node_parameters->declare_parameter("stats_enabled", rclcpp::ParameterValue());  
+  node_parameters->declare_parameter("stats_enabled", rclcpp::ParameterValue());  //declared stats_enabled with type PARAMETER_NOT_SET
   rclcpp::Parameter stats_enabled;
   node_parameters->get_parameter("stats_enabled", stats_enabled);
   
   if (((stats_enabled.get_type()!= rclcpp::PARAMETER_NOT_SET)&&(stats_enabled.as_bool()==true))||
-  ((stats_enabled.get_type()== rclcpp::PARAMETER_NOT_SET)&&(rclcpp::detail::resolve_enable_topic_statistics(options,*node_topics->get_node_base_interface()))))
+  ((stats_enabled.get_type()== rclcpp::PARAMETER_NOT_SET)&&(rclcpp::detail::resolve_enable_topic_statistics(options,*node_topics->get_node_base_interface())))) //run-time configuration has prioirty over code-configuration
   {	
-  	node_parameters->declare_parameter("stats_period", rclcpp::ParameterValue());
+  	node_parameters->declare_parameter("stats_period", rclcpp::ParameterValue());	//declared stats_period with type PARAMETER_NOT_SET
   	rclcpp::Parameter stats_period;
 		node_parameters->get_parameter("stats_period", stats_period);
 		
-	  if ((stats_period.get_type()!= rclcpp::PARAMETER_NOT_SET)&&(stats_period.as_double()<=0)) {
-			throw std::invalid_argument(
+		if ((stats_period.get_type()!= rclcpp::PARAMETER_NOT_SET)&&(stats_period.as_double()<=0)) {
+		throw std::invalid_argument(
 			          "stats_period must be greater than 0, specified value of " +
-			          std::to_string(stats_period.as_double()) +
-			          " ms");
+			          std::to_string(stats_period.as_double()) +" ms");
 	  } else if ((stats_period.get_type()== rclcpp::PARAMETER_NOT_SET)&&(options.topic_stats_options.publish_period <= std::chrono::milliseconds(0))) {
-	    throw std::invalid_argument(
+	    	throw std::invalid_argument(
 	            "topic_stats_options.publish_period must be greater than 0, specified value of " +
 	            std::to_string(options.topic_stats_options.publish_period.count()) +
 	            " ms");
 	  }
 	  
-		node_parameters->declare_parameter("stats_topic", rclcpp::ParameterValue());
+		node_parameters->declare_parameter("stats_topic", rclcpp::ParameterValue());	//declared stats_topic with type PARAMETER_NOT_SET
 		rclcpp::Parameter stats_topic;
 		node_parameters->get_parameter("stats_topic", stats_topic);
-          
-	  std::shared_ptr<Publisher<statistics_msgs::msg::MetricsMessage>> publisher =
-	    create_publisher<statistics_msgs::msg::MetricsMessage>(
-	    node,
-	    (stats_topic.get_type()!= rclcpp::PARAMETER_NOT_SET) ? stats_topic.as_string() : options.topic_stats_options.publish_topic, 
-	    qos);
+		        
+		std::shared_ptr<Publisher<statistics_msgs::msg::MetricsMessage>> publisher =
+			  create_publisher<statistics_msgs::msg::MetricsMessage>(
+			  node,
+			  (stats_topic.get_type()!= rclcpp::PARAMETER_NOT_SET) ? stats_topic.as_string() : options.topic_stats_options.publish_topic,
+			  qos);	//run-time configuration has prioirty over code-configuration
 
-	  subscription_topic_stats = std::make_shared<
-	    rclcpp::topic_statistics::SubscriptionTopicStatistics<CallbackMessageT>
-	    >(node_topics->get_node_base_interface()->get_name(), publisher);
+		subscription_topic_stats = std::make_shared<
+			  rclcpp::topic_statistics::SubscriptionTopicStatistics<CallbackMessageT>
+			  >(node_topics->get_node_base_interface()->get_name(), publisher);
 
-	  std::weak_ptr<
-	    rclcpp::topic_statistics::SubscriptionTopicStatistics<CallbackMessageT>
-	  > weak_subscription_topic_stats(subscription_topic_stats);
-	  auto sub_call_back = [weak_subscription_topic_stats]() {
-	      auto subscription_topic_stats = weak_subscription_topic_stats.lock();
-	      if (subscription_topic_stats) {
-	        subscription_topic_stats->publish_message();
-	      }
-	    };
+		std::weak_ptr<
+		  rclcpp::topic_statistics::SubscriptionTopicStatistics<CallbackMessageT>
+		> weak_subscription_topic_stats(subscription_topic_stats);
+		auto sub_call_back = [weak_subscription_topic_stats]() {
+		  auto subscription_topic_stats = weak_subscription_topic_stats.lock();
+		  if (subscription_topic_stats) {
+		    subscription_topic_stats->publish_message();
+		  }
+		};
 
-	  auto node_timer_interface = node_topics->get_node_timers_interface();
+		auto node_timer_interface = node_topics->get_node_timers_interface();
 
-	  auto timer = create_wall_timer(
-	    std::chrono::duration_cast<std::chrono::nanoseconds>(
-	      (stats_period.get_type()!= rclcpp::PARAMETER_NOT_SET) ? std::chrono::seconds(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::milliseconds((int)stats_period.as_double()*1000))) : options.topic_stats_options.publish_period),
-	    sub_call_back,
-	    options.callback_group,
-	    node_topics->get_node_base_interface(),
-	    node_timer_interface
-	  );
+		auto timer = create_wall_timer(
+			  std::chrono::duration_cast<std::chrono::nanoseconds>(
+			    (stats_period.get_type()!= rclcpp::PARAMETER_NOT_SET) ? 						
+			    std::chrono::seconds(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::milliseconds((int)stats_period.as_double()*1000))) : options.topic_stats_options.publish_period),
+			  sub_call_back,
+			  options.callback_group,
+			  node_topics->get_node_base_interface(),
+			  node_timer_interface
+			); //run-time configuration has prioirty over code-configuration
 
 	  subscription_topic_stats->set_publisher_timer(timer);
   }
