eddb938aec3287f585fed1607723681c583d2b1d
Chris Lalancette <clalancette@openrobotics.org>
2020-12-08 09:06:16 -0500

    Change nullptr checks to use ASSERT_TRUE. (#1486)
    * Change nullptr checks to use ASSERT_TRUE.
    
    clang static analysis gets a bit confused going through the
    gtest macros, so switch from ASSERT_NE to ASSERT_TRUE as
    we've done elsewhere.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rclcpp/test/rclcpp/allocator/test_allocator_deleter.cpp
+++ rclcpp/test/rclcpp/allocator/test_allocator_deleter.cpp
@@ -38,7 +38,9 @@ TEST(TestAllocatorDeleter, construct_destruct) {
 TEST(TestAllocatorDeleter, delete) {
   std::allocator<int> allocator;
   int * some_mem = allocator.allocate(1u);
-  ASSERT_NE(nullptr, some_mem);
+  // The more natural check here is ASSERT_NE(nullptr, ptr), but clang static
+  // analysis throws a false-positive memory leak warning.  Use ASSERT_TRUE instead.
+  ASSERT_TRUE(nullptr != some_mem);
 
   rclcpp::allocator::AllocatorDeleter<std::allocator<int>> deleter(&allocator);
   EXPECT_NO_THROW(deleter(some_mem));
