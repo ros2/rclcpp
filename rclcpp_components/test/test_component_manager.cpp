5745d246c78e955d9d0a5e7431ac3f90015213c0
Josh Langsfeld <josh.langsfeld@gmail.com>
2021-01-01 20:27:30 -0600

    Use std compliant non-method std::filesystem::exists function
    Signed-off-by: Josh Langsfeld <josh.langsfeld@gmail.com>

--- rclcpp_components/test/test_component_manager.cpp
+++ rclcpp_components/test/test_component_manager.cpp
@@ -51,9 +51,10 @@ TEST_F(TestComponentManager, get_component_resources_valid)
   EXPECT_EQ("test_rclcpp_components::TestComponentBar", resources[1].first);
   EXPECT_EQ("test_rclcpp_components::TestComponentNoNode", resources[2].first);
 
-  EXPECT_TRUE(rcpputils::fs::path(resources[0].second).exists());
-  EXPECT_TRUE(rcpputils::fs::path(resources[1].second).exists());
-  EXPECT_TRUE(rcpputils::fs::path(resources[2].second).exists());
+  namespace fs = rcpputils::fs;
+  EXPECT_TRUE(fs::exists(fs::path(resources[0].second)));
+  EXPECT_TRUE(fs::exists(fs::path(resources[1].second)));
+  EXPECT_TRUE(fs::exists(fs::path(resources[2].second)));
 }
 
 TEST_F(TestComponentManager, create_component_factory_valid)
