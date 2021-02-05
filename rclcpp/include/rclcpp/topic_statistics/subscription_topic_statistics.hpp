bce19675a2c28531ae591158445a7c896897f955
hsgwa <hasegawa@isp.co.jp>
2021-02-05 11:55:18 -0900

    clear statistics after window reset (#1531) (#1535)
    * clear statistics after window reset (#1531)
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * move ClearCurrentMeasurements just after GetStatisticsResults
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * refactor publish_message to publish_and_reset_message
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * rename function name
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * rename function name
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * add unit test to check window reset
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * pass measured == offset case
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * modify comment
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * add received message size test
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * rename test parameter names
    
    Signed-off-by: hsgwa <hasegawa@isp.co.jp>
    
    * set statistics collector's collection period explicitly to defaultStatisticsPublishPeriod
    
    Signed-off-by: Miaofei <miaofei@amazon.com>
    
    Co-authored-by: Miaofei <miaofei@amazon.com>

--- rclcpp/include/rclcpp/topic_statistics/subscription_topic_statistics.hpp
+++ rclcpp/include/rclcpp/topic_statistics/subscription_topic_statistics.hpp
@@ -127,7 +127,7 @@ public:
   /**
    * This method acquires a lock to prevent race conditions to collectors list.
    */
-  virtual void publish_message()
+  virtual void publish_message_and_reset_measurements()
   {
     std::vector<MetricsMessage> msgs;
     rclcpp::Time window_end{get_current_nanoseconds_since_epoch()};
@@ -136,6 +136,7 @@ public:
       std::lock_guard<std::mutex> lock(mutex_);
       for (auto & collector : subscriber_statistics_collectors_) {
         const auto collected_stats = collector->GetStatisticsResults();
+        collector->ClearCurrentMeasurements();
 
         auto message = libstatistics_collector::collector::GenerateStatisticMessage(
           node_name_,
