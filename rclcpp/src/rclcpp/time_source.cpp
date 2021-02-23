459aaae8fa03a80beafa5ab1cadc088920d462c9
anaelle-sw <anaelle.sarazin@wyca.fr>
2021-02-23 14:07:37 +0100

    fix code style divergence
    Signed-off-by: anaelle-sw <anaelle.sarazin@wyca.fr>
    Signed-off-by: anaelle <anaelle.sarazin@wyca.fr>

--- rclcpp/src/rclcpp/time_source.cpp
+++ rclcpp/src/rclcpp/time_source.cpp
@@ -263,8 +263,8 @@ void TimeSource::create_clock_sub()
 
   clock_executor_thread_ = std::thread(
     [this]() {
-        clock_executor_.add_callback_group(clock_callback_group_, node_base_);
-        clock_executor_.spin();
+      clock_executor_.add_callback_group(clock_callback_group_, node_base_);
+      clock_executor_.spin();
     }
   );
 }
