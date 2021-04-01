c1d4a84463bb5f1bdb30f2c61dee6d3c8f8adf79
Mauro Passerino <mpasserino@irobot.com>
2021-04-01 08:24:57 -0300

    Do not set gc listener callback
    Signed-off-by: Mauro Passerino <mpasserino@irobot.com>

--- rclcpp/src/rclcpp/subscription_intra_process_base.cpp
+++ rclcpp/src/rclcpp/subscription_intra_process_base.cpp
@@ -40,12 +40,6 @@ SubscriptionIntraProcessBase::set_listener_callback(
   rmw_listener_callback_t callback,
   const void * user_data) const
 {
-  rcl_ret_t ret = rcl_guard_condition_set_listener_callback(
-    &gc_,
-    callback,
-    user_data);
-
-  if (RCL_RET_OK != ret) {
-    throw std::runtime_error("Couldn't set guard condition listener callback");
-  }
+  (void)callback;
+  (void)user_data;
 }
