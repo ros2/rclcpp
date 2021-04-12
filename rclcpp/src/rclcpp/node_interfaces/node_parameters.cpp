512a4df61025d1346afc53cda631b9ef52fc50a3
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2021-04-12 11:40:49 -0300

    fix :joy:
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- rclcpp/src/rclcpp/node_interfaces/node_parameters.cpp
+++ rclcpp/src/rclcpp/node_interfaces/node_parameters.cpp
@@ -300,12 +300,13 @@ __set_parameters_atomically_common(
   bool allow_undeclared = false)
 {
   // Check if the value being set complies with the descriptor.
-  result = __check_parameters(parameter_infos, parameters, allow_undeclared);
+  rcl_interfaces::msg::SetParametersResult result = __check_parameters(
+    parameter_infos, parameters, allow_undeclared);
   if (!result.successful) {
     return result;
   }
   // Call the user callback to see if the new value(s) are allowed.
-  rcl_interfaces::msg::SetParametersResult result =
+  result =
     __call_on_parameters_set_callbacks(parameters, callback_container, callback);
   if (!result.successful) {
     return result;
