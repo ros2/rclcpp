f0d277866333a9a644b8886e97963c35e19afbb8
Christophe Bedard <bedard.christophe@gmail.com>
2021-08-09 14:01:14 -0400

    Keep publisher handle arg for rclcpp_publish tracepoint but use nullptr
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- rclcpp/include/rclcpp/publisher.hpp
+++ rclcpp/include/rclcpp/publisher.hpp
@@ -438,7 +438,7 @@ protected:
   void
   do_inter_process_publish(const ROSMessageType & msg)
   {
-    TRACEPOINT(rclcpp_publish, static_cast<const void *>(&msg));
+    TRACEPOINT(rclcpp_publish, nullptr, static_cast<const void *>(&msg));
     auto status = rcl_publish(publisher_handle_.get(), &msg, nullptr);
 
     if (RCL_RET_PUBLISHER_INVALID == status) {
