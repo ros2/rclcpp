3c29a572f72b6274a486f72116ca5667f0cfdba8
Gonzalo de Pedro <gonzalo@depedro.com.ar>
2021-12-30 17:10:27 -0300

    Added tests for non transform intra process
    Signed-off-by: Gonzalo de Pedro <gonzalo@depedro.com.ar>

--- rclcpp/test/rclcpp/test_subscription_publisher_with_same_type_adapter.cpp
+++ rclcpp/test/rclcpp/test_subscription_publisher_with_same_type_adapter.cpp
@@ -26,6 +26,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "rclcpp/msg/string.hpp"
+#include "statistics_msgs/msg/statistic_data_point.hpp"
 
 static const int g_max_loops = 200;
 static const std::chrono::milliseconds g_sleep_per_loop(10);
@@ -72,6 +73,31 @@ struct TypeAdapter<std::string, rclcpp::msg::String>
     (void) source;
     (void) destination;
     throw std::runtime_error("This should not happen");
+  }
+};
+
+template<>
+struct TypeAdapter<double, statistics_msgs::msg::StatisticDataPoint>
+{
+  using is_specialized = std::true_type;
+  using custom_type = double;
+  using ros_message_type = statistics_msgs::msg::StatisticDataPoint;
+
+  static void
+  convert_to_ros_message(
+    const custom_type & source,
+    ros_message_type & destination)
+  {
+    destination.data_type = 0;
+    destination.data = source;
+  }
+
+  static void
+  convert_to_custom(
+    const ros_message_type & source,
+    custom_type & destination)
+  {
+    destination = source.data;
   }
 };
 
@@ -508,6 +534,455 @@ TEST_F(
 
     std::string pu_message(message_data);
     pub->publish(pu_message);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+}
+
+TEST_F(
+  test_intra_process_within_one_node,
+  type_adapted_messages_ros_message_ref_pub_received_by_intra_process_subscription)
+{
+  using DoubleTypeAdapter = rclcpp::TypeAdapter<double, statistics_msgs::msg::StatisticDataPoint>;
+  const double message_data = 0.894;
+  const std::string topic_name = "topic_name";
+
+  auto node = rclcpp::Node::make_shared(
+    "test_intra_process",
+    rclcpp::NodeOptions().use_intra_process_comms(true));
+
+  auto pub = node->create_publisher<DoubleTypeAdapter>(topic_name, 1);
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::string &, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const double & msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::string & with message info, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const double & msg, const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::unique_ptr<std::string>, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::unique_ptr<double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::unique_ptr<std::string> with message info, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::unique_ptr<double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<std::string>, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<std::string> with message info, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<const std::string>, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<const double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<const std::string> with message info,
+    // publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<const double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::shared_ptr<const std::string> &, publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const std::shared_ptr<const double> & msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::shared_ptr<const std::string> & with message info,
+    // publish with rclcpp::msg::String &
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const std::shared_ptr<const double> & msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    statistics_msgs::msg::StatisticDataPoint msg;
+    msg.data = message_data;
+    pub->publish(msg);
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+}
+
+TEST_F(
+  test_intra_process_within_one_node,
+  type_adapted_messages_unique_ptr_ros_message_pub_received_by_intra_process_subscription)
+{
+  using DoubleTypeAdapter = rclcpp::TypeAdapter<double, statistics_msgs::msg::StatisticDataPoint>;
+  const double message_data = 0.7508;
+  const std::string topic_name = "topic_name";
+
+  auto node = rclcpp::Node::make_shared(
+    "test_intra_process",
+    rclcpp::NodeOptions().use_intra_process_comms(true));
+
+  auto pub = node->create_publisher<DoubleTypeAdapter>(topic_name, 1);
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::string &, publish with unique statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const double & msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::string & with message info, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const double & msg, const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::unique_ptr<std::string>, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::unique_ptr<double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::unique_ptr<std::string> with message info, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::unique_ptr<double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<std::string>, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<std::string> with message info, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<const std::string>, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<const double> msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback std::shared_ptr<const std::string> with message info, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      std::shared_ptr<const double> msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::shared_ptr<const std::string> &, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const std::shared_ptr<const double> & msg) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
+
+    ASSERT_FALSE(is_received);
+    wait_for_message_to_be_received(is_received, node);
+    ASSERT_TRUE(is_received);
+  }
+  { // create_publisher with TypeAdapt struct, create_subscription with TypeAdapt struct,
+    // callback const std::shared_ptr<const std::string> & with message info, publish with unique
+    // statistics_msgs::msg::StatisticDataPoint
+    bool is_received = false;
+    auto callback =
+      [message_data, &is_received](
+      const std::shared_ptr<const double> & msg,
+      const rclcpp::MessageInfo & message_info) -> void {
+        is_received = true;
+        ASSERT_EQ(message_data, *msg);
+        ASSERT_TRUE(message_info.get_rmw_message_info().from_intra_process);
+      };
+    auto sub = node->create_subscription<DoubleTypeAdapter>(topic_name, 1, callback);
+
+    auto pu_msg = std::make_unique<statistics_msgs::msg::StatisticDataPoint>();
+    pu_msg->data = message_data;
+    pub->publish(std::move(pu_msg));
 
     ASSERT_FALSE(is_received);
     wait_for_message_to_be_received(is_received, node);
