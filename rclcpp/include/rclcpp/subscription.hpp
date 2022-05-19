e52732a395444b2580e855036b62012081c62016
Barry Xu <barry.xu@sony.com>
2022-05-19 23:10:17 +0800

    Add statistics for handle_loaned_message (#1927)
    * Add statistics for handle_loaned_message
    
    Signed-off-by: Barry Xu <barry.xu@sony.com>
    (cherry picked from commit 5c688303b3cb994969f448979f64c12971243295)
    
    # Conflicts:
    #	rclcpp/include/rclcpp/subscription.hpp

--- rclcpp/include/rclcpp/subscription.hpp
+++ rclcpp/include/rclcpp/subscription.hpp
@@ -287,11 +287,38 @@ public:
     void * loaned_message,
     const rclcpp::MessageInfo & message_info) override
   {
+<<<<<<< HEAD
     auto typed_message = static_cast<CallbackMessageT *>(loaned_message);
     // message is loaned, so we have to make sure that the deleter does not deallocate the message
     auto sptr = std::shared_ptr<CallbackMessageT>(
       typed_message, [](CallbackMessageT * msg) {(void) msg;});
+=======
+    if (matches_any_intra_process_publishers(&message_info.get_rmw_message_info().publisher_gid)) {
+      // In this case, the message will be delivered via intra process and
+      // we should ignore this copy of the message.
+      return;
+    }
+
+    auto typed_message = static_cast<ROSMessageType *>(loaned_message);
+    // message is loaned, so we have to make sure that the deleter does not deallocate the message
+    auto sptr = std::shared_ptr<ROSMessageType>(
+      typed_message, [](ROSMessageType * msg) {(void) msg;});
+
+    std::chrono::time_point<std::chrono::system_clock> now;
+    if (subscription_topic_statistics_) {
+      // get current time before executing callback to
+      // exclude callback duration from topic statistics result.
+      now = std::chrono::system_clock::now();
+    }
+
+>>>>>>> 5c688303 (Add statistics for handle_loaned_message (#1927))
     any_callback_.dispatch(sptr, message_info);
+
+    if (subscription_topic_statistics_) {
+      const auto nanos = std::chrono::time_point_cast<std::chrono::nanoseconds>(now);
+      const auto time = rclcpp::Time(nanos.time_since_epoch().count());
+      subscription_topic_statistics_->handle_message(*typed_message, time);
+    }
   }
 
   /// Return the borrowed message.
