e1efffb8ee844be0885634c47f87e164f3cb1802
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2022-08-30 17:43:42 -0300

    Revert "Revert "Add a `create_timer` method to `Node` and `LifecycleNode` classes (#1975)" (#2009)"
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
    
    This reverts commit 11b5f8db2191f7921364f88cd20c04a80526121a.

--- rclcpp_lifecycle/test/test_lifecycle_publisher.cpp
+++ rclcpp_lifecycle/test/test_lifecycle_publisher.cpp
@@ -42,10 +42,17 @@ protected:
   }
 };
 
+/// We want to test everything for both the wall and generic timer.
+enum class TimerType
+{
+  WALL_TIMER,
+  GENERIC_TIMER,
+};
+
 class EmptyLifecycleNode : public rclcpp_lifecycle::LifecycleNode
 {
 public:
-  explicit EmptyLifecycleNode(const std::string & node_name)
+  explicit EmptyLifecycleNode(const std::string & node_name, const TimerType & timer_type)
   : rclcpp_lifecycle::LifecycleNode(node_name)
   {
     rclcpp::PublisherOptionsWithAllocator<std::allocator<void>> options;
@@ -55,8 +62,20 @@ public:
     add_managed_entity(publisher_);
 
     // For coverage this is being added here
-    auto timer = create_wall_timer(std::chrono::seconds(1), []() {});
-    add_timer_handle(timer);
+    switch (timer_type) {
+      case TimerType::WALL_TIMER:
+        {
+          auto timer = create_wall_timer(std::chrono::seconds(1), []() {});
+          add_timer_handle(timer);
+          break;
+        }
+      case TimerType::GENERIC_TIMER:
+        {
+          auto timer = create_timer(std::chrono::seconds(1), []() {});
+          add_timer_handle(timer);
+          break;
+        }
+    }
   }
 
   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<test_msgs::msg::Empty>> publisher()
@@ -68,13 +87,13 @@ private:
   std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<test_msgs::msg::Empty>> publisher_;
 };
 
-class TestLifecyclePublisher : public ::testing::Test
+class TestLifecyclePublisher : public ::testing::TestWithParam<TimerType>
 {
 public:
   void SetUp()
   {
     rclcpp::init(0, nullptr);
-    node_ = std::make_shared<EmptyLifecycleNode>("node");
+    node_ = std::make_shared<EmptyLifecycleNode>("node", GetParam());
   }
 
   void TearDown()
@@ -86,7 +105,7 @@ protected:
   std::shared_ptr<EmptyLifecycleNode> node_;
 };
 
-TEST_F(TestLifecyclePublisher, publish_managed_by_node) {
+TEST_P(TestLifecyclePublisher, publish_managed_by_node) {
   // transition via LifecycleNode
   auto success = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::SUCCESS;
   auto reset_key = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn::ERROR;
@@ -125,7 +144,7 @@ TEST_F(TestLifecyclePublisher, publish_managed_by_node) {
   }
 }
 
-TEST_F(TestLifecyclePublisher, publish) {
+TEST_P(TestLifecyclePublisher, publish) {
   // transition via LifecyclePublisher
   node_->publisher()->on_deactivate();
   EXPECT_FALSE(node_->publisher()->is_activated());
@@ -148,3 +167,19 @@ TEST_F(TestLifecyclePublisher, publish) {
     EXPECT_NO_THROW(node_->publisher()->publish(std::move(msg_ptr)));
   }
 }
+
+INSTANTIATE_TEST_SUITE_P(
+  PerTimerType, TestLifecyclePublisher,
+  ::testing::Values(TimerType::WALL_TIMER, TimerType::GENERIC_TIMER),
+  [](const ::testing::TestParamInfo<TimerType> & info) -> std::string {
+    switch (info.param) {
+      case TimerType::WALL_TIMER:
+        return std::string("wall_timer");
+      case TimerType::GENERIC_TIMER:
+        return std::string("generic_timer");
+      default:
+        break;
+    }
+    return std::string("unknown");
+  }
+);
