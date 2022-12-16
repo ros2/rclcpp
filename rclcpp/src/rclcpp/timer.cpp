43dee48350bce795c6553f760a05d8c8bf0f8516
Mauro Passerino <mpasserino@irobot.com>
2022-12-16 11:15:55 -0300

    Update cpp_callback_trampoline on timer.cpp
    Signed-off-by: Mauro Passerino <mpasserino@irobot.com>

--- rclcpp/src/rclcpp/timer.cpp
+++ rclcpp/src/rclcpp/timer.cpp
@@ -179,7 +179,8 @@ TimerBase::set_on_reset_callback(std::function<void(size_t)> callback)
   // This two-step setting, prevents a gap where the old std::function has
   // been replaced but rcl hasn't been told about the new one yet.
   set_on_reset_callback(
-    rclcpp::detail::cpp_callback_trampoline<const void *, size_t>,
+    rclcpp::detail::cpp_callback_trampoline<
+      decltype(new_callback), const void *, size_t>,
     static_cast<const void *>(&new_callback));
 
   // Store the std::function to keep it in scope, also overwrites the existing one.
@@ -187,7 +188,8 @@ TimerBase::set_on_reset_callback(std::function<void(size_t)> callback)
 
   // Set it again, now using the permanent storage.
   set_on_reset_callback(
-    rclcpp::detail::cpp_callback_trampoline<const void *, size_t>,
+    rclcpp::detail::cpp_callback_trampoline<
+      decltype(on_reset_callback_), const void *, size_t>,
     static_cast<const void *>(&on_reset_callback_));
 }
 
