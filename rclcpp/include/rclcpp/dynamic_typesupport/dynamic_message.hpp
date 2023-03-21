9c1371d2c5328b830ce2d9380b2b1d5515bf2b84
methylDragon <methylDragon@gmail.com>
2023-03-21 16:04:30 -0700

    Delete redundant wrapper class definitions
    Signed-off-by: methylDragon <methylDragon@gmail.com>

--- rclcpp/include/rclcpp/dynamic_typesupport/dynamic_message.hpp
+++ rclcpp/include/rclcpp/dynamic_typesupport/dynamic_message.hpp
@@ -34,70 +34,11 @@ namespace rclcpp
 namespace dynamic_typesupport
 {
 
-using DynamicMessageType = DynamicType;
-/// Thin wrapper around DynamicData object for message pubsub
-/**
- * This class:
- * - Manages the lifetime of the raw pointer.
- * - Exposes getter methods to get the raw pointer and shared pointers
- * - Exposes the underlying serialization support API
- *
- * Ownership:
- * - This class borrows the rosidl_dynamic_typesupport_serialization_support_t stored in the passed
- *   DynamicSerializationSupport. So it cannot outlive the DynamicSerializationSupport.
- * - The DynamicSerializationSupport's rosidl_dynamic_typesupport_serialization_support_t pointer
- *   must point to the same location in memory as the stored raw pointer!
- */
 
-// class DynamicMessage final : public DynamicData
-// {
-// public:
-//   RCLCPP_SMART_PTR_ALIASES_ONLY(DynamicMessage)
-//
-//   // CONSTRUCTION ==================================================================================
-//   // Most constructors require a passed in DynamicSerializationSupport::SharedPtr, to extend the
-//   // lifetime of the serialization support (if the constructor cannot otherwise get it from args).
-//   //
-//   // In cases where a dynamic data pointer is passed, the serialization support composed by
-//   // the data should be the exact same object managed by the DynamicSerializationSupport,
-//   // otherwise the lifetime management will not work properly.
-//
-//   /// Construct a new DynamicMessage with the provided dynamic type builder
-//   RCLCPP_PUBLIC
-//   explicit DynamicMessage(DynamicTypeBuilder::SharedPtr dynamic_type_builder);
-//
-//   /// Construct a new DynamicMessage with the provided dynamic type
-//   RCLCPP_PUBLIC
-//   explicit DynamicMessage(DynamicType::SharedPtr dynamic_type);
-//
-//   /// Assume ownership of raw pointer
-//   RCLCPP_PUBLIC
-//   DynamicMessage(
-//     DynamicSerializationSupport::SharedPtr serialization_support,
-//     rosidl_dynamic_typesupport_dynamic_data_t * rosidl_dynamic_data);
-//
-//   /// Copy shared pointer
-//   RCLCPP_PUBLIC
-//   DynamicMessage(
-//     DynamicSerializationSupport::SharedPtr serialization_support,
-//     std::shared_ptr<rosidl_dynamic_typesupport_dynamic_data_t> rosidl_dynamic_data);
-//
-//   /// Loaning constructor
-//   /// Must only be called with raw ptr obtained from loaning!
-//   // NOTE(methylDragon): I'd put this in protected, but I need this exposed to
-//   //                     enable_shared_from_this...
-//   RCLCPP_PUBLIC
-//   DynamicMessage(
-//     DynamicData::SharedPtr parent_data,
-//     rosidl_dynamic_typesupport_dynamic_data_t * rosidl_loaned_data);
-//
-//   // NOTE(methylDragon): Deliberately no constructor from description to nudge users towards using
-//   //                     construction from dynamic type/builder, which is more efficient
-//
-// private:
-//   RCLCPP_PUBLIC
-//   DynamicMessage();
-// };
+// NOTE(methylDragon): We just alias the type in this case...
+//                     I'd have made a wrapper class but then I'd need to redirect every single
+//                     method (or dynamic cast everywhere else), so.. no thanks.
+using DynamicMessageType = DynamicType;
 
 
 }  // namespace dynamic_typesupport
