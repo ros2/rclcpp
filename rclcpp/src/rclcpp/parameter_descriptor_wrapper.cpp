474e4616e16f4ee922e40b99c92eae7e86536ecf
CursedRock17 <mtglucas1@gmail.com>
2023-05-03 21:15:35 -0400

    Fitting in the cpp file
    Signed-off-by: CursedRock17 <mtglucas1@gmail.com>

--- rclcpp/src/rclcpp/parameter_descriptor_wrapper.cpp
+++ rclcpp/src/rclcpp/parameter_descriptor_wrapper.cpp
@@ -4,81 +4,109 @@
 
 namespace rclcpp {
 // We use initializer lists in order to promote safety in uninitialzied state
-ParameterDescription(std::string name, std::uint8 type, std::string description, std::string additional_constraints, bool read_only. bool dynamic_typing) : m_name{name}, parameter_descriptor.type{type}, m_description{description}, m_additional_constraints{additional_constraints}, read_only{read_only}, dyanmic_typing{m_dyanmic_typing}
+    ParameterDescription::ParameterDescription(std::string name, std::uint8 type, std::string description, std::string additional_constraints, bool read_only. bool dynamic_typing) : m_name{name}, parameter_descriptor.type{type}, m_description{description}, m_additional_constraints{additional_constraints}, read_only{read_only}, dyanmic_typing{m_dyanmic_typing}
 {}
 
+ParameterDescription::ParameterDescription(){}
 
-// ParameterDescription - FloatingPointDescription
+// ParameterDescription - ParameterDescription
 // First the build methods to connect to the base class in the builder
-ParameterDescription FloatingPointDescription::build() const
+ParameterDescription ParameterDescription::build() const
 {
     return ParameterDescription(m_name, parameter_descriptor.type, m_description, m_additional_constraints, m_read_only, m_dynamic_typing)
 }
 
 // Builder methods which set up the original class
 // They all follow the same format of initing the value given within the base class, then returning this current class
-FloatingPointDescription& FloatingPointDescription::SetName(std::string name)
+ParameterDescription& ParameterDescription::SetName(const std::string& name)
 {
     m_name = name;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetType(std::uint8_t type)
+ParameterDescription& ParameterDescription::SetType(std::uint8_t type)
 {
     parameter_descriptor.type = type;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetDescription(std::string description)
+ParameterDescription& ParameterDescription::SetDescriptionText(const std::string& description)
 {
     m_description = description;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetAdditionalConstraints(std::string constraints)
+ParameterDescription& ParameterDescription::SetAdditionalConstraints(const std::string& constraints)
 {
     m_additional_constraints = constraints;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetReadOnly(bool read_only)
+ParameterDescription& ParameterDescription::SetReadOnly(bool read_only)
 {
     m_read_only = read_only;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetDynamicTyping(bool dynamic_typing)
+ParameterDescription& ParameterDescription::SetDynamicTyping(bool dynamic_typing)
 {
     m_dynamic_typing = dynamic_typing;
     return *this;
 }
 
 // These are the extension for this class that don't have access in the main class so we'll initialize here
-FloatingPointDescription& FloatingPointDescription::SetMin(float min)
+ParameterDescription& ParameterDescription::SetMin(float min)
 {
-    m_min = min;
+    m_min_float = min;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetMax(float max)
+ParameterDescription& ParameterDescription::SetMax(float max)
 {
-    m_max = max;
+    m_max_float = max;
     return *this;
 }
 
-FloatingPointDescription& FloatingPointDescription::SetStep(float step)
+ParameterDescription& ParameterDescription::SetStep(float step)
 {
-    m_step = step;
+    m_step_float = step;
+    return *this;
+}
+
+ParameterDescription& ParameterDescription::SetMin(int min)
+{
+    m_min_int = min;
+    return *this;
+}
+
+ParameterDescription& ParameterDescription::SetMax(int max)
+{
+    m_max_int = max;
+    return *this;
+}
+
+ParameterDescription& ParameterDescription::SetStep(int step)
+{
+    m_step_int = step;
     return *this;
 }
 
 // Here is the Specific range function for this parameter description
-FloatingPointDescription& FloatingPointDescription::SetFloatingPointDescripion(float min, float max, float step)
+ParameterDescription& ParameterDescription::SetFloatingPointDescriptionRange(float min, float max, float step)
 {
         parameter_descriptor.floating_point_range.resize(1);
         parameter_descriptor.floating_point_range.at(0).from_value = min;
         parameter_descriptor.floating_point_range.at(0).to_value = max;
         parameter_descriptor.floating_point_range.at(0).step = step;
+        return *this;
+}
+
+ParameterDescription& ParameterDescription::SetIntegerDescriptionRange(int min, int max, int step)
+{
+        parameter_descriptor.integer_range.resize(1);
+        parameter_descriptor.integer_range.at(0).from_value = min;
+        parameter_descriptor.integer_range.at(0).to_value = max;
+        parameter_descriptor.integer_range.at(0).step = step;
         return *this;
 }
 
