a8e279c268337215b801f833bdbdee3b7c21cd88
Joshua Hampp <j.hampp@denso-adas.de>
2023-12-11 15:52:34 +0100

    added test for SubscriptionIntraProcess with unique subscriber
    Signed-off-by: Joshua Hampp <j.hampp@denso-adas.de>

--- rclcpp/test/rclcpp/test_intra_process_subscriber.cpp
+++ rclcpp/test/rclcpp/test_intra_process_subscriber.cpp
@@ -167,6 +167,70 @@ TEST_F(TestIntraProcessSubscriber, methods_shared) {
   EXPECT_EQ(3u, counter);
 }
 
+/*
+   Test methods of SubscriptionIntraProcess with unique callback:
+    * is_serialized
+    * deserialize_message
+    * serve_serialized_message
+ */
+TEST_F(TestIntraProcessSubscriber, methods_unique) {
+  using MessageT = test_msgs::msg::Empty;
+  using Alloc = std::allocator<void>;
+
+  // create IPM subscriber
+  uint32_t counter = 0u;
+  rclcpp::AnySubscriptionCallback<MessageT, Alloc> callback;
+  callback.set(
+    [&counter](std::unique_ptr<MessageT>) {
+      ++counter;
+    });
+
+  auto allocator = std::make_shared<Alloc>();
+  auto context = rclcpp::contexts::get_global_default_context();
+  auto subscriber = std::make_shared<rclcpp::experimental::SubscriptionIntraProcess<MessageT,
+      MessageT>>(
+    callback,
+    allocator,
+    context,
+    "topic",
+    rclcpp::SystemDefaultsQoS().keep_last(10),
+    rclcpp::IntraProcessBufferType::UniquePtr);
+
+  EXPECT_NE(nullptr, subscriber);
+  EXPECT_EQ(false, subscriber->is_serialized());
+
+  // generate a serialized message
+  MessageT msg;
+  rclcpp::SerializedMessage serialized_message;
+  rclcpp::Serialization<MessageT> serializer;
+  serializer.serialize_message(&msg, &serialized_message);
+
+  // test deserialization
+  auto deserialized_msg = subscriber->deserialize_message(&serialized_message);
+  EXPECT_EQ(msg, *deserialized_msg);
+
+  // Get the intra process manager instance for this context.
+  auto ipm = context->get_sub_context<rclcpp::experimental::IntraProcessManager>();
+  const auto sub_id = ipm->add_subscription(subscriber);
+  auto publisher = node->create_publisher<MessageT>("topic", 42);
+  const auto intra_process_publisher_id = ipm->add_publisher(publisher);
+
+  std::vector<uint64_t> take_ownership_subscriptions = {sub_id};
+  std::vector<uint64_t> take_shared_subscriptions;
+  subscriber->serve_serialized_message(
+    &serialized_message,
+    ipm.get(),
+    intra_process_publisher_id,
+    reinterpret_cast<void *>(allocator.get()),
+    take_ownership_subscriptions,
+    take_shared_subscriptions);
+
+  // execute subscriber callback
+  {
+    auto data = subscriber->take_data();
+    subscriber->execute(data);
+  }
+
   // check if message was received
   EXPECT_EQ(1u, counter);
 }
