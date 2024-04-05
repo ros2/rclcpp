17929cea8966c7e39e0920e6c7d8012d11c70426
Matthias Holoch <matthias.holoch@naturerobots.com>
2024-04-05 16:24:35 +0200

    change docs to express what is possible instead of what is not
    Co-authored-by: Tomoya Fujita <Tomoya.Fujita@sony.com>
    Signed-off-by: Matthias Holoch <matthias.holoch@naturerobots.com>

--- rclcpp/include/rclcpp/executor.hpp
+++ rclcpp/include/rclcpp/executor.hpp
@@ -347,7 +347,7 @@ public:
 
   /// Spin (blocking) until the future is complete, it times out waiting, or rclcpp is interrupted.
   /**
-   * Handles one work item after the other, hence, it does not support parallel processing.
+   * The implementation is suitable for a single-thread model of execution.
    * \param[in] future The future to wait on. If this function returns SUCCESS, the future can be
    *   accessed without blocking (though it may still throw an exception).
    * \param[in] timeout Optional timeout parameter, which gets passed to Executor::spin_node_once.
