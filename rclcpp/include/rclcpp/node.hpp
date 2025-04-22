a705cccc7859a4153afcabdcf9fa5926b4910acb
Tomoya Fujita <Tomoya.Fujita@sony.com>
2025-04-21 17:58:30 -0700

    typo fixes.
    Signed-off-by: Tomoya Fujita <Tomoya.Fujita@sony.com>

--- rclcpp/include/rclcpp/node.hpp
+++ rclcpp/include/rclcpp/node.hpp
@@ -1559,9 +1559,9 @@ public:
    * which has been created using an existing instance of this class, but which
    * has an additional sub-namespace (short for subordinate namespace)
    * associated with it.
-   * A suboardinate node and an instance of this class share all the node interfaces
+   * A subordinate node and an instance of this class share all the node interfaces
    * such as `rclcpp::node_interfaces::NodeParameterInterface`.
-   * Suboardinate nodes are primarily used to organize namespaces and provide a
+   * Subordinate nodes are primarily used to organize namespaces and provide a
    * hierarchical structure, but they are not meant to be completely independent nodes.
    * The sub-namespace will extend the node's namespace for the purpose of
    * creating additional entities, such as Publishers, Subscriptions, Service
