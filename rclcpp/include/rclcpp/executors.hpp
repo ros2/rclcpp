eb36918c3c85c850ca917294a7a08fc552993803
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-05-01 11:09:56 +0200

    Removed deprecated StaticSingleThreadedExecutor
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rclcpp/include/rclcpp/executors.hpp
+++ rclcpp/include/rclcpp/executors.hpp
@@ -20,7 +20,6 @@
 
 #include "rclcpp/executors/multi_threaded_executor.hpp"
 #include "rclcpp/executors/single_threaded_executor.hpp"
-#include "rclcpp/executors/static_single_threaded_executor.hpp"
 #include "rclcpp/experimental/executors/events_executor/events_executor.hpp"
 #include "rclcpp/node.hpp"
 #include "rclcpp/utilities.hpp"
--- rclcpp/test/rclcpp/executors/test_executors_timer_cancel_behavior.cpp
+++ rclcpp/test/rclcpp/executors/test_executors_timer_cancel_behavior.cpp
@@ -285,20 +285,11 @@ public:
   T executor;
 };
 
-#if !defined(_WIN32)
-# ifdef __clang__
-#  pragma clang diagnostic push
-#  pragma clang diagnostic ignored "-Wdeprecated-declarations"
-# endif
-#endif
 using MainExecutorTypes =
   ::testing::Types<
   rclcpp::executors::SingleThreadedExecutor,
-  rclcpp::executors::MultiThreadedExecutor,
-  DeprecatedStaticSingleThreadedExecutor>;
-#ifdef __clang__
-# pragma clang diagnostic pop
-#endif
+  rclcpp::executors::MultiThreadedExecutor>;
+
 // TODO(@fujitatomoya): this test excludes EventExecutor because it does not
 // support simulation time used for this test to relax the racy condition.
 // See more details for https://github.com/ros2/rclcpp/issues/2457.
