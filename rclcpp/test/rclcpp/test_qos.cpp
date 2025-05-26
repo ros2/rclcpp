9f8a8f9516f9b587273ee351444ad20819f24b55
Janosch Machowinski <jmachowinski@users.noreply.github.com>
2025-05-26 17:27:35 +0200

    Update rclcpp/test/rclcpp/test_qos.cpp
    Signed-off-by: Janosch Machowinski <jmachowinski@users.noreply.github.com>

--- rclcpp/test/rclcpp/test_qos.cpp
+++ rclcpp/test/rclcpp/test_qos.cpp
@@ -278,7 +278,7 @@ TEST(TestQoS, from_rmw_validity)
   rmw_qos_profile_t invalid_qos;
   memset(&invalid_qos, 0, sizeof(invalid_qos));
   unsigned int n = 999;
-  memcpy(&invalid_qos.history, &n, sizeof(unsigned int));
+  memcpy(&invalid_qos.history, &n, sizeof(n));
 
   EXPECT_THROW({
     rclcpp::QoSInitialization::from_rmw(invalid_qos);
