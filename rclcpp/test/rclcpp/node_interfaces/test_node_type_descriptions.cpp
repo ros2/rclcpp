677d9a17a15d2fc7c1e268c88ee811cc76895513
Patrick Roncagliolo <ronca.pat@gmail.com>
2025-07-10 09:03:59 +0200

    Add test
    Signed-off-by: Patrick Roncagliolo <ronca.pat@gmail.com>

--- rclcpp/test/rclcpp/node_interfaces/test_node_type_descriptions.cpp
+++ rclcpp/test/rclcpp/node_interfaces/test_node_type_descriptions.cpp
@@ -49,6 +49,17 @@ TEST_F(TestNodeTypeDescriptions, automatically_declare_parameters_from_overrides
   });
 }
 
+TEST_F(TestNodeTypeDescriptions, bad_parameter_type)
+{
+  rclcpp::NodeOptions node_options;
+  node_options.append_parameter_override("start_type_description_service", "unexpected_type");
+  ASSERT_THROW(
+  {
+    auto node = std::make_shared<rclcpp::Node>("node", "ns", node_options);
+    (void) node;
+  }, std::invalid_argument);
+}
+
 TEST_F(TestNodeTypeDescriptions, disabled_no_service)
 {
   rclcpp::NodeOptions node_options;
