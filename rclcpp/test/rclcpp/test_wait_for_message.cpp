258703abec0b9fd3f1a94b735e345f7b88f9d510
Sriharsha Ghanta <ghanta1996@gmail.com>
2025-07-13 14:36:34 +0800

    fix: cpplint
    Signed-off-by: Sriharsha Ghanta <ghanta1996@gmail.com>
    Signed-off-by: Sriharsha Ghanta <harsha@lionsbot.com>
    Signed-off-by: Sriharsha Ghanta <ghanta1996@gmail.com>

--- rclcpp/test/rclcpp/test_wait_for_message.cpp
+++ rclcpp/test/rclcpp/test_wait_for_message.cpp
@@ -110,7 +110,6 @@ TEST(TestUtilities, wait_for_message_twice_one_sub) {
 }
 
 TEST(TestUtilities, wait_for_last_message){
-
   rclcpp::init(0, nullptr);
 
   auto node = std::make_shared<rclcpp::Node>("wait_for_last_message_node");
