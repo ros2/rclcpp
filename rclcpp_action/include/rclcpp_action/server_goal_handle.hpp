6397047d4795f594cf65dd360d70f5c9c3618700
Andrei Costinescu <AndreiCostinescu@users.noreply.github.com>
2026-01-20 09:57:19 +0100

    Update exception documentation for goal cancellation in ServerGoalHandle (#3019)
    * Update exception documentation for goal cancellation
    
    The documentation for the canceled function is misleading.
    Previously, the description said: 
    1. "Only call this if the goal is canceling." and 
    2. "\throws rclcpp::exceptions::RCLError If the goal is in any state besides executing."
    This is a contradiction.
    Experimentally verified that if the goal is executing and this method is called, an error is thrown. This makes the second statement wrong => correct the statement in the documentation.
    
    Signed-off-by: Andrei Costinescu <AndreiCostinescu@users.noreply.github.com>

--- rclcpp_action/include/rclcpp_action/server_goal_handle.hpp
+++ rclcpp_action/include/rclcpp_action/server_goal_handle.hpp
@@ -210,7 +210,7 @@ public:
    * This is a terminal state, no more methods should be called on a goal handle after this is
    * called.
    *
-   * \throws rclcpp::exceptions::RCLError If the goal is in any state besides executing.
+   * \throws rclcpp::exceptions::RCLError If a cancel request for this goal has not been received.
    *
    * \param[in] result_msg the final result to send to clients.
    */
