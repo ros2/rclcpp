99da9c55f50e351630e4c6ac171843dda90688f1
Rahat Dhande <rahatchd@gmail.com>
2026-01-19 22:35:29 -0800

    construct wait set with passed in context

--- rclcpp/include/rclcpp/wait_for_message.hpp
+++ rclcpp/include/rclcpp/wait_for_message.hpp
@@ -56,7 +56,7 @@ bool wait_for_message(
       }
     });
 
-  rclcpp::WaitSet wait_set;
+  rclcpp::WaitSet wait_set({}, {}, {}, {}, {}, {}, context);
   wait_set.add_subscription(subscription);
   RCPPUTILS_SCOPE_EXIT(wait_set.remove_subscription(subscription); );
   wait_set.add_guard_condition(gc);
