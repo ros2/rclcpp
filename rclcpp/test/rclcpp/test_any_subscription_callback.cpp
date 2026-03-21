af78e01bd63933aaa255677277581f069dc9f0d6
Alexis Tsogias <1114095+Zyrin@users.noreply.github.com>
2026-03-21 05:06:30 +0100

    remove duplicate test cases in TestAnySubscriptionCallback::is_serialized_message_callback (#3104)
    Signed-off-by: Ubuntu <ubuntu@ubuntu-2404-ros-rolling.lxd>
    Co-authored-by: Ubuntu <ubuntu@ubuntu-2404-ros-rolling.lxd>

--- rclcpp/test/rclcpp/test_any_subscription_callback.cpp
+++ rclcpp/test/rclcpp/test_any_subscription_callback.cpp
@@ -113,15 +113,6 @@ TEST_F(TestAnySubscriptionCallback, is_serialized_message_callback) {
   }
   {
     rclcpp::AnySubscriptionCallback<test_msgs::msg::Empty> asc;
-    asc.set([](const rclcpp::SerializedMessage &, const rclcpp::MessageInfo &) {});
-    EXPECT_TRUE(asc.is_serialized_message_callback());
-    EXPECT_NO_THROW(
-      asc.dispatch(
-        std::make_shared<rclcpp::SerializedMessage>(),
-        rclcpp::MessageInfo{}));
-  }
-  {
-    rclcpp::AnySubscriptionCallback<test_msgs::msg::Empty> asc;
     asc.set([](std::unique_ptr<rclcpp::SerializedMessage>) {});
     EXPECT_TRUE(asc.is_serialized_message_callback());
     EXPECT_NO_THROW(
@@ -171,24 +162,6 @@ TEST_F(TestAnySubscriptionCallback, is_serialized_message_callback) {
       [](
         const std::shared_ptr<const rclcpp::SerializedMessage> &,
         const rclcpp::MessageInfo &) {});
-    EXPECT_TRUE(asc.is_serialized_message_callback());
-    EXPECT_NO_THROW(
-      asc.dispatch(
-        std::make_shared<rclcpp::SerializedMessage>(),
-        rclcpp::MessageInfo{}));
-  }
-  {
-    rclcpp::AnySubscriptionCallback<test_msgs::msg::Empty> asc;
-    asc.set([](std::shared_ptr<const rclcpp::SerializedMessage>) {});
-    EXPECT_TRUE(asc.is_serialized_message_callback());
-    EXPECT_NO_THROW(
-      asc.dispatch(
-        std::make_shared<rclcpp::SerializedMessage>(),
-        rclcpp::MessageInfo{}));
-  }
-  {
-    rclcpp::AnySubscriptionCallback<test_msgs::msg::Empty> asc;
-    asc.set([](std::shared_ptr<const rclcpp::SerializedMessage>, const rclcpp::MessageInfo &) {});
     EXPECT_TRUE(asc.is_serialized_message_callback());
     EXPECT_NO_THROW(
       asc.dispatch(
