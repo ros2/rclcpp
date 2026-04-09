c29150e73d96d33bf5a6ad95ab947f0694a8b188
Skyler Medeiros <skye@polymathrobotics.com>
2026-04-08 17:22:45 -0700

    add callback group events executor component container
    Signed-off-by: Skyler Medeiros <skye@polymathrobotics.com>

---
+++ rclcpp_components/src/component_container_cbg.cpp
@@ -0,0 +1,42 @@
+// Copyright 2026 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+
+#include "rclcpp/utilities.hpp"
+#include "rclcpp/executors/events_cbg_executor/events_cbg_executor.hpp"
+
+#include "rclcpp_components/component_manager.hpp"
+
+int main(int argc, char * argv[])
+{
+  /// Component container with an events executor.
+  rclcpp::init(argc, argv);
+  rclcpp::executors::EventsCBGExecutor::SharedPtr exec = nullptr;
+
+  auto node = std::make_shared<rclcpp_components::ComponentManager>();
+
+  if (node->has_parameter("thread_num")) {
+    const auto thread_num = node->get_parameter("thread_num").as_int();
+    exec = std::make_shared<rclcpp::executors::EventsCBGExecutor>(
+      rclcpp::ExecutorOptions{}, thread_num);
+  } else {
+    exec = std::make_shared<rclcpp::executors::EventsCBGExecutor>();
+  }
+  node->set_executor(exec);
+  exec->add_node(node);
+  exec->spin();
+
+  return 0;
+}
