98e22c4840d79a67638efc2e4e3e55fae7027b7d
Skyler Medeiros <skye@polymathrobotics.com>
2026-04-08 17:23:32 -0700

    Add EventsExecutor and EventsCBGExecutor to isolated component container
    Signed-off-by: Skyler Medeiros <skye@polymathrobotics.com>

--- rclcpp_components/src/component_container_isolated.cpp
+++ rclcpp_components/src/component_container_isolated.cpp
@@ -13,39 +13,73 @@
 // limitations under the License.
 
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include "rclcpp/executors/single_threaded_executor.hpp"
 #include "rclcpp/executors/multi_threaded_executor.hpp"
+#include "rclcpp/experimental/executors/events_executor/events_executor.hpp"
+#include "rclcpp/executors/events_cbg_executor/events_cbg_executor.hpp"
 #include "rclcpp/utilities.hpp"
 
 #include "rclcpp_components/component_manager_isolated.hpp"
 
 int main(int argc, char * argv[])
 {
-  /// Component container with dedicated single-threaded executors for each components.
+  /// Component container with dedicated executor for each component
   rclcpp::init(argc, argv);
+
   // parse arguments
-  bool use_multi_threaded_executor{false};
+  // valid entries: --executor-type single-threaded, --executor-type multi-threaded, --executor-type events
   std::vector<std::string> args = rclcpp::remove_ros_arguments(argc, argv);
-  for (auto & arg : args) {
-    if (arg == std::string("--use_multi_threaded_executor")) {
-      use_multi_threaded_executor = true;
+  rclcpp::Node::SharedPtr node;
+
+  std::string executor_type = "single-threaded";  // default
+  for (size_t i = 0; i < args.size(); ++i) {
+    if (args[i] == "--executor-type") {
+      if (i + 1 < args.size()) {
+        executor_type = args[i + 1];
+        break;
+      }
+    } else if (args[i] == "--use_multi_threaded_executor") { // backward compatibility
+      RCLCPP_WARN(node->get_logger(),
+        "--use_multi_threaded_executor is deprecated, use --executor-type multi-threaded instead.");
+      executor_type = "multi-threaded";
     }
   }
+
   // create executor and component manager
-  auto exec = std::make_shared<rclcpp::executors::SingleThreadedExecutor>();
-  rclcpp::Node::SharedPtr node;
-  if (use_multi_threaded_executor) {
-    using ComponentManagerIsolated =
-      rclcpp_components::ComponentManagerIsolated<rclcpp::executors::MultiThreadedExecutor>;
+  std::shared_ptr<rclcpp::Executor> exec;
+  if (executor_type == "events") {
+    using executor = rclcpp::experimental::executors::EventsExecutor;
+    using ComponentManagerIsolated = rclcpp_components::ComponentManagerIsolated<executor>;
+    exec = std::make_shared<executor>();
+    node = std::make_shared<ComponentManagerIsolated>(exec);
+  } else if (executor_type == "cbg-single-threaded") {
+    using executor = rclcpp::executors::EventsCBGExecutor;
+    using ComponentManagerIsolated = rclcpp_components::ComponentManagerIsolated<executor>;
+    exec = std::make_shared<executor>(rclcpp::ExecutorOptions(), 1);
+    node = std::make_shared<ComponentManagerIsolated>(exec);
+  } else if (executor_type == "cbg-multi-threaded") {
+    using executor = rclcpp::executors::EventsCBGExecutor;
+    using ComponentManagerIsolated = rclcpp_components::ComponentManagerIsolated<executor>;
+    exec = std::make_shared<executor>();
+    node = std::make_shared<ComponentManagerIsolated>(exec);
+  } else if (executor_type == "multi-threaded") {
+    using executor = rclcpp::executors::MultiThreadedExecutor;
+    using ComponentManagerIsolated = rclcpp_components::ComponentManagerIsolated<executor>;
+    exec = std::make_shared<executor>();
+    node = std::make_shared<ComponentManagerIsolated>(exec);
+  } else if (executor_type == "single-threaded") {
+    using executor = rclcpp::executors::SingleThreadedExecutor;
+    using ComponentManagerIsolated = rclcpp_components::ComponentManagerIsolated<executor>;
+    exec = std::make_shared<executor>();
     node = std::make_shared<ComponentManagerIsolated>(exec);
   } else {
-    using ComponentManagerIsolated =
-      rclcpp_components::ComponentManagerIsolated<rclcpp::executors::SingleThreadedExecutor>;
-    node = std::make_shared<ComponentManagerIsolated>(exec);
+    std::cerr << "Invalid executor type: " << executor_type << std::endl;
+    return 1;
   }
+
   exec->add_node(node);
   exec->spin();
 
