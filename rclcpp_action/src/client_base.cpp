c85d28eb723ff958d6cd614ccbc22aeafc3dc744
Ivo Ivanov <ivo.ivanov@tha.de>
2026-06-24 21:38:43 +0200

    Fixed race condition on the RNG in rclcpp_action
    Signed-off-by: Ivo Ivanov <ivo.ivanov@tha.de>

--- rclcpp_action/src/client_base.cpp
+++ rclcpp_action/src/client_base.cpp
@@ -181,6 +181,7 @@ public:
   std::map<int64_t, ResponseCallback> pending_cancel_responses;
   std::recursive_mutex cancel_requests_mutex;
 
+  std::mutex goal_id_rng_mutex;
   std::independent_bits_engine<
     std::mt19937, 8, unsigned int> random_bytes_generator;
 };
@@ -480,6 +481,7 @@ GoalUUID
 ClientBase::generate_goal_id()
 {
   GoalUUID goal_id;
+  std::lock_guard<std::mutex> lock(pimpl_->goal_id_rng_mutex);
   // TODO(hidmic): Do something better than this for UUID generation.
   // std::generate(
   //   goal_id.uuid.begin(), goal_id.uuid.end(),
